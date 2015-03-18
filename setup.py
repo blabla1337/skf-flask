7e1b3e4a16460e02892d5d4b0249e8e1c20d940a
blabla1337 <glenntencate@gmail.com>
2015-03-18 23:18:07 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ except ImportError:
     from distutils.core import setup
 
 setup(name='owasp-skf',
-    version='0.2.5a3',
+    version='0.2.6a3',
     description='The OWASP Security Knowledge Framework',
     url='https://github.com/blabla1337/skf-flask',
     author='Glenn ten Cate, Riccardo ten Cate',
