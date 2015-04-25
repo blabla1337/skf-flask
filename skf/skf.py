5bd03afcc56ad76c93f1caf62abf0544752843a2
Erik de Kuijper <erikdekuijper@gmail.com>
2015-04-25 23:58:28 +0200

    Style Changes

--- skf/skf.py
+++ skf/skf.py
@@ -139,11 +139,11 @@ csrf_token = base64.b64encode(csrf_token_raw)
 # You can also replace password with static password:  PASSWORD='pass!@#example'
 app.config.update(dict(
     DATABASE=os.path.join(app.root_path, 'skf.db'),
-    DEBUG=True,
+    DEBUG=False,
     SECRET_KEY=secret_key,
     USERNAME='admin',
     SESSION_COOKIE_SECURE=True,
-    PASSWORD='test123',
+    PASSWORD=password,
     SESSION_COOKIE_HTTPONLY = True
 ))
 
