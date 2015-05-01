c72813562b7f1872deed477c9c2e44800d1b0131
Erik de Kuijper <erikdekuijper@gmail.com>
2015-05-01 17:44:48 +0200

    Style Changes

--- skf/static/js/common-scripts.js
+++ skf/static/js/common-scripts.js
@@ -71,9 +71,9 @@ var Script = function () {
     });
 
 // custom scrollbar
-    $("#sidebar").niceScroll({styler:"fb",cursorcolor:"#4ECDC4", cursorwidth: '3', cursorborderradius: '10px', background: '#404040', spacebarenabled:false, cursorborder: ''});
+    $("#sidebar").niceScroll({styler:"fb",cursorcolor:"#515594", cursorwidth: '3', cursorborderradius: '10px', background: '#494949', spacebarenabled:false, cursorborder: ''});
 
-    $("html").niceScroll({styler:"fb",cursorcolor:"#4ECDC4", cursorwidth: '6', cursorborderradius: '10px', background: '#404040', spacebarenabled:false,  cursorborder: '', zindex: '1000'});
+    $("html").niceScroll({styler:"fb",cursorcolor:"#515594", cursorwidth: '6', cursorborderradius: '10px', background: '#494949', spacebarenabled:false,  cursorborder: '', zindex: '1000'});
 
 // widget tools
 
