27fb819ce2a31bfee9bc1728f2fca42f3959281e
Erik de Kuijper <erikdekuijper@gmail.com>
2015-05-10 11:53:59 +0200

    Minor Changes

--- skf/static/js/actions.js
+++ skf/static/js/actions.js
@@ -28,71 +28,10 @@ var Script = function () {
             $("#sidebar").scrollTo("+="+Math.abs(diff),500);
     });
 
-
-
-//    sidebar toggle
-
-    $(function() {
-        function responsiveView() {
-            var wSize = $(window).width();
-            if (wSize <= 768) {
-                $('#container').addClass('sidebar-close');
-                $('#sidebar > ul').hide();
-            }
-
-            if (wSize > 768) {
-                $('#container').removeClass('sidebar-close');
-                $('#sidebar > ul').show();
-            }
-        }
-        $(window).on('load', responsiveView);
-        $(window).on('resize', responsiveView);
-    });
-
-    $('.fa-bars').click(function () {
-        if ($('#sidebar > ul').is(":visible") === true) {
-            $('#main-content').css({
-                'margin-left': '0px'
-            });
-            $('#sidebar').css({
-                'margin-left': '-210px'
-            });
-            $('#sidebar > ul').hide();
-            $("#container").addClass("sidebar-closed");
-        } else {
-            $('#main-content').css({
-                'margin-left': '210px'
-            });
-            $('#sidebar > ul').show();
-            $('#sidebar').css({
-                'margin-left': '0'
-            });
-            $("#container").removeClass("sidebar-closed");
-        }
-    });
-
 // custom scrollbar
     $("#sidebar").niceScroll({styler:"fb",cursorcolor:"#515594", cursorwidth: '3', cursorborderradius: '10px', background: '#494949', spacebarenabled:false, cursorborder: ''});
 
     $("html").niceScroll({styler:"fb",cursorcolor:"#515594", cursorwidth: '6', cursorborderradius: '10px', background: '#494949', spacebarenabled:false,  cursorborder: '', zindex: '1000'});
-
-// widget tools
-
-    jQuery('.panel .tools .fa-chevron-down').click(function () {
-        var el = jQuery(this).parents(".panel").children(".panel-body");
-        if (jQuery(this).hasClass("fa-chevron-down")) {
-            jQuery(this).removeClass("fa-chevron-down").addClass("fa-chevron-up");
-            el.slideUp(200);
-        } else {
-            jQuery(this).removeClass("fa-chevron-up").addClass("fa-chevron-down");
-            el.slideDown(200);
-        }
-    });
-
-    jQuery('.panel .tools .fa-times').click(function () {
-        jQuery(this).parents(".panel").parent().remove();
-    });
-
 
 //    tool tips
 
@@ -162,6 +101,8 @@ $(document).ready(function() {
 		$(this).toggleClass('act');
 	});
 	
+	// Instant Search
+	
     $('input[name=search]').keyup(function () {
         var things = $("#accordion div.panel");
         var val = $(this).val();
@@ -173,10 +114,16 @@ $(document).ready(function() {
         });
     });
 	
+	// Alphabetize Items
+	
 	var $divs = $("div.panel");
     var orderedDivs = $divs.sort(function (a, b) {
         return $(a).find("h4").text() > $(b).find("h4").text();
     });
     $("#accordion").html(orderedDivs);
+	
+	// Hide "Not Available Item" in Knowledge Base
+	
+	$("#accordion .panel a:contains('not available item')").parent().parent().parent().remove();
 	
 });
