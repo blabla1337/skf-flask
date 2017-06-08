edd5d4f4d1308ce477219e843a009eca37771f2c
blabla1337 <glenntencate@gmail.com>
2017-06-08 12:00:31 +0200

    fix for docker?

--- skf/app.py
+++ skf/app.py
@@ -110,7 +110,7 @@ def main():
     """Main SKF method"""
     initialize_app(app)
     log.info('>>>>> Starting development server at http://{}/api/ <<<<<'.format(app.config['SERVER_NAME']))
-    app.run(debug=settings.FLASK_DEBUG)
+    app.run(host=settings.FLASK_HOST, port=settings.FLASK_PORT, debug=settings.FLASK_DEBUG)
 
 
 if __name__ == "__main__":
--- skf/settings.py
+++ skf/settings.py
@@ -1,5 +1,7 @@
 # Flask settings
-FLASK_SERVER_NAME = '0.0.0.0:8888'
+FLASK_HOST = '0.0.0.0'
+FLASK_PORT = '8888'
+FLASK_SERVER_NAME = FLASK_HOST+":"+FLASK_PORT
 FLASK_DEBUG = True  # Do not use debug mode in production
 
 # Flask-Restplus settings
