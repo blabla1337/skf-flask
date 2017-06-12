395580537aa9aede45883372b64f9ae5a273b91d
blabla1337 <glenntencate@gmail.com>
2017-06-12 21:33:27 +0200

    fixed bug checklists

--- skf/api/checklist/business.py
+++ skf/api/checklist/business.py
@@ -7,6 +7,7 @@ from skf.database.checklists_kb import checklists_kb
 def get_checklist_item(checklist_id):
     log("User requested specific checklist item", "LOW", "PASS")
     val_float(checklist_id)
+   
     result = checklists_kb.query.filter(checklists_kb.checklistID == checklist_id).one()
     return result
     
@@ -20,6 +21,11 @@ def get_checklist_items():
 def get_checklist_items_lvl(lvl):
     log("User requested list of checklist items based on level", "LOW", "PASS")
     val_num(lvl)
-    result = checklists_kb.query.filter(checklists_kb.checklist_items.has(level = 0) | checklists_kb.checklist_items.has(level = lvl)).group_by(checklists_kb.checklistID).paginate(1, 1500, False)
+    if lvl == 1:
+        result = checklists_kb.query.filter(checklists_kb.checklist_items.has(level = 0) | checklists_kb.checklist_items.has(level = 1)).group_by(checklists_kb.checklistID).paginate(1, 1500, False)
+    elif lvl == 2:
+        result = checklists_kb.query.filter(checklists_kb.checklist_items.has(level = 0) | checklists_kb.checklist_items.has(level = 1) | checklists_kb.checklist_items.has(level = 2)).group_by(checklists_kb.checklistID).paginate(1, 1500, False)
+    elif lvl == 3:
+        result = checklists_kb.query.filter(checklists_kb.checklist_items.has(level = 0) | checklists_kb.checklist_items.has(level = 1) | checklists_kb.checklist_items.has(level = 2) | checklists_kb.checklist_items.has(level = 3)).group_by(checklists_kb.checklistID).paginate(1, 1500, False)
     return result
 
