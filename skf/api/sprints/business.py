8f9133d3e477dc6200134099376f8dfb11f2fbf8
blabla1337 <glenntencate@gmail.com>
2017-06-12 20:47:43 +0200

    fixed bug, duplicates in results

--- skf/api/sprints/business.py
+++ skf/api/sprints/business.py
@@ -18,7 +18,7 @@ def get_sprint_results(sprint_id, user_id):
     log("User requested specific sprint items", "MEDIUM", "PASS")
     val_num(sprint_id)
     val_num(user_id)
-    result = checklists_results.query.filter(checklists_results.sprintID == sprint_id).order_by(desc(checklists_results.status)).paginate(1, 500, False)
+    result = checklists_results.query.filter(checklists_results.sprintID == sprint_id).group_by(checklists_results.checklistID).order_by(desc(checklists_results.status)).paginate(1, 500, False)
     return result
 
 
@@ -26,7 +26,7 @@ def get_sprint_results_audit(sprint_id, user_id):
     log("User requested specific sprint audit items", "MEDIUM", "PASS")
     val_num(sprint_id)
     val_num(user_id)
-    result = checklists_results.query.filter(checklists_results.sprintID == sprint_id).filter(checklists_results.status == 5).paginate(1, 500, False)
+    result = checklists_results.query.filter(checklists_results.sprintID == sprint_id).filter(checklists_results.status == 5).group_by(checklists_results.checklistID).paginate(1, 500, False)
     return result
 
 
