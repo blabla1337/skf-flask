8b2731b17fcd617f5525992eb53ac78c4af85eea
blabla1337 <glenntencate@gmail.com>
2017-06-17 12:54:46 +0200

    fix karma run 1x only and exit

--- Angular/karma.conf.js
+++ Angular/karma.conf.js
@@ -41,7 +41,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    singleRun: false,
+    singleRun: true,
 
     customLaunchers: {
         Chrome_travis_ci: {
