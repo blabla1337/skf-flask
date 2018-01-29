01e505dbf11f9d6144bd9cd75102db4853f1f6f7
Martin Marsicano <martin.marsicano@owasp.org>
2018-01-28 21:17:00 -0300

    Order in checklist page
    Order requirements in checklist page

--- skf/api/checklist/business.py
+++ skf/api/checklist/business.py
@@ -10,12 +10,12 @@ def get_checklist_item(checklist_id):
    
     result = checklists_kb.query.filter(checklists_kb.checklistID == checklist_id).one()
     return result
-    
+
 
 def get_checklist_items():
     log("User requested list of checklist items", "LOW", "PASS")
     result = checklists_kb.query.group_by(checklists_kb.checklistID).paginate(1, 1500, False)
-    return result
+    return order_checklist_items(result)
 
 
 def get_checklist_items_lvl(lvl):
@@ -27,5 +27,34 @@ def get_checklist_items_lvl(lvl):
         result = checklists_kb.query.filter(checklists_kb.checklist_items.has(level = 0) | checklists_kb.checklist_items.has(level = 1) | checklists_kb.checklist_items.has(level = 2)).group_by(checklists_kb.checklistID).paginate(1, 1500, False)
     elif lvl == 3:
         result = checklists_kb.query.filter(checklists_kb.checklist_items.has(level = 0) | checklists_kb.checklist_items.has(level = 1) | checklists_kb.checklist_items.has(level = 2) | checklists_kb.checklist_items.has(level = 3)).group_by(checklists_kb.checklistID).paginate(1, 1500, False)
-    return result
+    return order_checklist_items(result)
+
+
+def order_checklist_items(checklist_items):
+    ordered_checklist_items = []
+    for item in checklist_items.items:
+        numbers = item.checklistID.split('.')
+        category = int(numbers[0])
+        category_requirement = int(numbers[1])
+        if (len(ordered_checklist_items) == 0):
+            ordered_checklist_items.append(item)
+        else:
+            y = 0
+            while y < len(ordered_checklist_items):
+                numbers_ordered = ordered_checklist_items[y].checklistID.split('.')
+                category_ordered = int(numbers_ordered[0])
+                category_requirement_ordered = int(numbers_ordered[1])
+                if (category < category_ordered):
+                    ordered_checklist_items.insert(y, item)
+                    break
+                else:
+                    if (category == category_ordered):
+                        if (category_requirement < category_requirement_ordered):
+                            ordered_checklist_items.insert(y, item)
+                            break
+                y = y + 1
+            if (y == len(ordered_checklist_items)):
+                ordered_checklist_items.insert(y, item)
+    checklist_items.items = ordered_checklist_items
+    return checklist_items
 
