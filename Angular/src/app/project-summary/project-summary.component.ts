45f0fb29f2bce4426ff8cb0640df6715d92f4279
Martin Marsicano <martin.marsicano@owasp.org>
2018-02-05 12:21:44 -0300

    Fix reordening issue 428
    Using observable in Angular (thanks RiieCco) with finally making the call for the new list.

--- Angular/src/app/project-summary/project-summary.component.ts
+++ Angular/src/app/project-summary/project-summary.component.ts
@@ -49,17 +49,19 @@ export class ProjectSummaryComponent implements OnInit {
 
   save(status: number, checklist: string) {
     this.route.params.subscribe(params => {
+      if (this.comment == undefined || this.comment === "") {
+        this.comment = "";
+      }
       this.commentService.newComment(checklist, this.comment, params['id'], status).subscribe(
         () => { },
         err => console.log("Error whilst storing the comment!"),
-        () => { this.succes = "comment was added to trail"; }
+        () => { 
+          this.succes = "comment was added to trail"; 
+          this.sprintService.getSprintResults(params['id']).subscribe(
+            resp => this.sprintResult = resp,
+            err => console.log("Error getting sprint stats"))
+        }
       )
-    });
-
-    this.route.params.subscribe(params => {
-      this.sprintService.getSprintResults(params['id']).subscribe(
-        resp => this.sprintResult = resp,
-        err => console.log("Error getting sprint stats"))
     });
     this.comment = "";
   }
