4a1e74f3b3ac90d3e863d2d11e01d3135ccdd4f8
Leena Bhegade <bhegde.leena@gmail.com>
2019-01-28 12:19:53 +0000

    changes

--- Angular/server.js
+++ Angular/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static(path.join(__dirname + 'dist')));
 
 app.get('/*', function(req,res) {
-	res.sendFile(path.join(__dirname+'dist/index.html'));
+	res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
 const PORT = 8080;
--- Docker/alpine/skf-angular.sh
+++ Docker/alpine/skf-angular.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Start the SKF Angular app
+ls
 cd /skf-flask/Angular
 ls
 node server.js
