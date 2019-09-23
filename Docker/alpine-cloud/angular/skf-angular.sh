ca7e18077c4e08f3a4a0892c68a835a347ff55a6
Glenn ten Cate <glenntencate@gmail.com>
2019-09-23 19:36:03 +0200

    Update skf-angular.sh

--- Docker/alpine-cloud/angular/skf-angular.sh
+++ Docker/alpine-cloud/angular/skf-angular.sh
@@ -5,4 +5,4 @@ ORIGIN=${ORIGIN:-'localhost'}
 
 # Start the SKF Angular app
 cd /home/user_api/skf-flask/Angular
-ng serve --configuration=production --publicHost $ORIGIN
+ng serve --configuration=production --host 0.0.0.0 --publicHost $ORIGIN
