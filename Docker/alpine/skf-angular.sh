74ab09c06597ffbb65efbc4fff7c4aa08286d33a
blabla1337 <glenntencate@gmail.com>
2020-02-17 14:24:06 +0100

    fix skf Docker local

--- Docker/alpine/skf-angular.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -x
-ORIGIN=${ORIGIN:-'localhost'}
-
-# Start the SKF Angular app
-cd /home/user_skf/Angular
-ng serve --configuration=production  --host 0.0.0.0 --publicHost $ORIGIN
