f9f47e337002f8d8c28307faf5b2ac4407aa41f2
Glenn ten Cate <glenntencate@gmail.com>
2020-04-06 11:30:07 -0700

    minor merge fixes + unit tests updated

--- skf/api/code/endpoints/code_item_delete.py
+++ skf/api/code/endpoints/code_item_delete.py
@@ -1,4 +1,3 @@
-
 from flask import request
 from flask_restplus import Resource
 from skf.api.security import security_headers, validate_privilege, select_userid_jwt
@@ -6,7 +5,6 @@ from skf.api.code.business import delete_code_item
 from skf.api.code.serializers import message
 from skf.api.code.parsers import authorization
 from skf.api.restplus import api
-from skf.api.security import log, val_num, val_alpha, val_alpha_num, val_alpha_num_special
 
 ns = api.namespace('code', description='Operations related to knowledge base')
 
@@ -24,9 +22,7 @@ class KnowledgebaseItemDelete(Resource):
         Deletes code item.
         * Privileges required: **delete**
         """
-        val_num(id)
         validate_privilege(self, 'delete')
-        result = delete_code_item(id)
+        user_id = select_userid_jwt(self)
+        result = delete_code_item(id, user_id)
         return result, 200, security_headers()
-
-
