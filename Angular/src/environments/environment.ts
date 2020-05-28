6a66fa86ce50de3b9912c9f8521a66e165c674d7
Glenn ten Cate <glenntencate@gmail.com>
2020-05-28 15:48:09 +0200

    fix angular api call

--- Angular/src/environments/environment.ts
+++ Angular/src/environments/environment.ts
@@ -3,7 +3,7 @@
 export const environment = {
   production: false,
   //DO NOT CHANGE HTTP TO HTTPS MANUALLY SHOULD BE HANDLED BY DEPLOY SCRIPTS
-  API_ENDPOINT: 'http://localhost:8888/api',
+  API_ENDPOINT: 'http://127.0.0.1:8888/api',
   //AUTH_METHOD: 'openidprovider'
   //AUTH_METHOD: 'skiploginprovider'
   AUTH_METHOD: 'skfprovider'
