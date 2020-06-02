c6f1ecf9804d8b0a607541cd7aff35e3fc50576a
Glenn ten Cate <glenntencate@gmail.com>
2020-06-02 14:35:54 +0200

    update filenames for windows uesrs

--- Angular/src/environments/environment.ts
+++ Angular/src/environments/environment.ts
@@ -1,7 +1,11 @@
 // The file contents for the current environment will overwrite these during build.
 
 export const environment = {
-  production: false,
-  API_ENDPOINT: 'https://owasp-skf.cloud.tyk.io/api/api',
-  AUTH_METHOD: 'openidprovider'
+  production: true,
+  //DO NOT CHANGE HTTP TO HTTPS MANUALLY SHOULD BE HANDLED BY DEPLOY SCRIPTS
+  API_ENDPOINT: 'http://127.0.0.1:8888/api', 
+  //API_ENDPOINT: 'https://owasp-skf.cloud.tyk.io/api/api',
+  //AUTH_METHOD: 'openidprovider'
+  //AUTH_METHOD: 'skiploginprovider'
+  AUTH_METHOD: 'skfprovider'
 };
