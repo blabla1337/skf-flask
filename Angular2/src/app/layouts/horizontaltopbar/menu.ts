bef115bcfb7a664161282a328c6b39a4a2be58d4
Akash M <38563661+AkashM398@users.noreply.github.com>
2020-06-09 00:24:18 +0530

    Changed Color Schemes and Modified the Dashboard (#596)
    * Topbar on progress
    
    * Add global var and func
    
    * completed toolbar
    
    * Completed Toolbar
    
    * completed dashboard & removed md files
    
    * Fixed reponsiveness
    
    * Changed color scheme

--- Angular2/src/app/layouts/horizontaltopbar/menu.ts
+++ Angular2/src/app/layouts/horizontaltopbar/menu.ts
@@ -1,30 +1,33 @@
 import { MenuItem } from './menu.model';
+import { UseExistingWebDriver } from 'protractor/built/driverProviders';
+import { userInfo } from 'os';
 
 export const MENU: MenuItem[] = [
     {
         id: 1,
         label: 'Dashboards',
-        icon: 'bx-home-circle',
+        icon: 'mdi mdi-18px mdi-view-dashboard mr-2',
     },
     {
         id: 2,
         label: 'Manage Projects',
-        icon: 'bx-tone',
+        icon: 'mdi mdi-18px mdi-card-bulleted mr-2',
         isUiElement: true,
     },
     {
         id: 3,
+        icon: 'mdi mdi-18px mdi-account-multiple mr-2',
         label: 'Users',
     },
     {
         id: 4,
-        icon: 'bx-collection',
+        icon: 'mdi mdi-18px mdi-text-box-check mr-2',
         label: 'Checklists',
     },
     {
         id: 5,
         label: 'Code examples',
-        icon: 'bx-file',
+        icon: 'mdi mdi-18px mdi-code-greater-than-or-equal mr-2',
     }
 ];
 
