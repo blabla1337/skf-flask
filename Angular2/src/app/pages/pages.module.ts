076d91134035b4acc824424881342c0d64d2533f
Akash M <38563661+AkashM398@users.noreply.github.com>
2020-06-13 20:59:03 +0530

    Added Users Page (#599)
    * Added Button, Updated alignments
    
    * Added Card Contents
    
    * Added helper classes and removed unwanted code
    
    * Added CSS to User Page
    
    * Completed Users Page
    
    * Changes in CSS

--- Angular2/src/app/pages/pages.module.ts
+++ Angular2/src/app/pages/pages.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { UIModule } from '../shared/ui/ui.module';
 
 import { PagesRoutingModule } from './pages-routing.module';
 import { DashboardModule } from './dashboard/dashboard.module';
+import { ManageComponent } from './users/manage/manage.component';
+import { NgbNavModule, NgbDropdownModule, NgbModalModule, NgbTooltipModule, NgbPaginationModule, NgbTypeaheadModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ManageComponent],
   imports: [
     CommonModule,
     DashboardModule,
-    PagesRoutingModule
+    PagesRoutingModule,
+    NgbPaginationModule,
+    NgbNavModule,
+    NgbDropdownModule,
+    FormsModule,
+    UIModule
   ],
   providers: []
 })
