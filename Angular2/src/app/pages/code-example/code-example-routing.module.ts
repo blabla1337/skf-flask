1f374e507c55662748478d49adba6e1184c404a5
Akash M <38563661+AkashM398@users.noreply.github.com>
2020-06-15 20:02:49 +0530

    Completed the UI of Code Examples (#611)
    * Added Button, Updated alignments
    
    * Added Card Contents
    
    * Added helper classes and removed unwanted code
    
    * Added CSS to User Page
    
    * Completed Users Page
    
    * Changes in CSS
    
    * Created Code Example Page
    
    * added code routing
    
    * Fixed Code Example Routing
    
    * Completed the UI for Code Examples

--- Angular2/src/app/pages/code-example/code-example-routing.module.ts
+++ Angular2/src/app/pages/code-example/code-example-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import {CodeExampleComponent} from './code-example.component';
+import {ViewComponent} from './view/view.component';
 
-const routes: Routes = [ {path: 'manage', component: CodeExampleComponent}];
+const routes: Routes = [ {path: 'view', component: ViewComponent}];
 
 @NgModule({
     imports: [RouterModule.forChild(routes)],
     exports: [RouterModule]
 })
-export class CodeExampleRouting {}
+export class CodeExampleRoutingModule {}
