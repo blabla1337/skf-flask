1e86cc5d158759e2787fe2e49d0e699166d9ec70
Akash M <38563661+AkashM398@users.noreply.github.com>
2020-06-15 20:02:49 +0530

    Completed the UI of Code Examples (#611)
    * Added Button, Updated alignments
    
    * Added Card Contents
    
    * Added helper classes and removed unwanted code
    
    * Added CSS to User Page
    
    * Completed Users Page
    
    * Changes in CSS
    
    * Created Code Example Page
    
    * added code routing
    
    * Fixed Code Example Routing
    
    * Completed the UI for Code Examples

---
+++ Angular2/src/app/pages/code-example/view/view.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Codes } from './code-example.model';
+
+import { codeData } from './data';
+
+@Component({
+  selector: 'app-view',
+  templateUrl: './view.component.html',
+  styleUrls: ['./view.component.scss']
+})
+export class ViewComponent implements OnInit {
+
+  // bread crumb items
+  breadCrumbItems: Array<{}>;
+
+  codeData: Codes[];
+
+
+  // Collapse declare
+  isCollapsed: boolean;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.breadCrumbItems = [{ label: 'Code Examples' }, { label: 'View', active: true }];
+
+    // Collapse value
+    this.isCollapsed = false;
+
+    this._fetchData();
+  }
+
+  /**
+   * Code data fetches
+   */
+  private _fetchData() {
+    this.codeData = codeData;
+  }
+}
