6d9b411c8e4337fc99dc8df14c433c167eb47b92
Akash M <akashm1999@gmail.com>
2020-06-21 23:07:03 +0530

    Implented Checklist View

--- Angular2/src/app/layouts/horizontaltopbar/horizontaltopbar.component.ts
+++ Angular2/src/app/layouts/horizontaltopbar/horizontaltopbar.component.ts
@@ -42,8 +42,6 @@ export class HorizontaltopbarComponent implements OnInit, AfterViewInit {
     };
   }
 
-  
-
   /**
    * On menu click
    */
---
+++ Angular2/src/app/pages/checklists/data.ts
@@ -0,0 +1,46 @@
+const checkData = [
+    {
+        id: 1,
+        text: 'Checklists Name 1',
+        subtext: 'This is the description for the checklists',
+        status: 'Active',
+        date: '15 Jun, 20',
+    },
+    {
+        id: 2,
+        text: 'Checklists Name 1',
+        subtext: 'This is the description for the checklists',
+        status: 'Active',
+        date: '15 Jun, 20',
+    },
+    {
+        id: 3,
+        text: 'Checklists Name 1',
+        subtext: 'This is the description for the checklists',
+        status: 'Active',
+        date: '15 Jun, 20',
+    },
+    {
+        id: 4,
+        text: 'Checklists Name 1',
+        subtext: 'This is the description for the checklists',
+        status: 'Active',
+        date: '15 Jun, 20',
+    },
+    {
+        id: 5,
+        text: 'Checklists Name 1',
+        subtext: 'This is the description for the checklists',
+        status: 'Active',
+        date: '15 Jun, 20',
+    },
+    {
+        id: 6,
+        text: 'Checklists Name 1',
+        subtext: 'This is the description for the checklists',
+        status: 'Active',
+        date: '15 Jun, 20',
+    },
+];
+
+export { checkData };
