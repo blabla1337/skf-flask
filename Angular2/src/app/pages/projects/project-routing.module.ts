953ca1470aa2fbf131005a281dddb315554ea6fa
Akash M <akashm1999@gmail.com>
2020-06-22 23:32:00 +0530

    Implemented Project Page

--- Angular2/src/app/pages/projects/project-routing.module.ts
+++ Angular2/src/app/pages/projects/project-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import {ManageComponent} from './manage/manage.component';
+import {ProjectManageComponent} from './manage/manage.component';
 
-const routes: Routes = [ {path: 'manage', component: ManageComponent}];
+const routes: Routes = [ {path: 'manage', component: ProjectManageComponent}];
 
 @NgModule({
     imports: [RouterModule.forChild(routes)],
