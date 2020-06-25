32ba4faee48d4ab9b4071e4640388c3290cbe5a7
Akash M <38563661+AkashM398@users.noreply.github.com>
2020-06-25 23:15:56 +0530

    Login Page Completed and Updated Layout (#619)
    * Added Button, Updated alignments
    
    * Added Card Contents
    
    * Added helper classes and removed unwanted code
    
    * Added CSS to User Page
    
    * Completed Users Page
    
    * Changes in CSS
    
    * Created Code Example Page
    
    * added code routing
    
    * Fixed Code Example Routing
    
    * Completed the UI for Code Examples
    
    * Knowledgebase Created & Added Routing
    
    * fixed collapse
    
    * Modifies Knowledgebase files
    
    * implemented knowledgebase design
    
    * created checklists
    
    * Added Routing
    
    * implementing dynamic theme
    
    * Implented Checklist View
    
    * removed unwanted CSS and fixed Checklists View
    
    * Created Projects Page
    
    * Implemented Project Page
    
    * Added Login Page
    
    * Added Login Routing
    
    * completed login page and fixed layout

--- Angular2/src/app/pages/auth/login/login.component.ts
+++ Angular2/src/app/pages/auth/login/login.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  year: number = new Date().getFullYear();
+
   constructor() { }
 
   ngOnInit(): void {
