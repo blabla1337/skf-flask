99af960d65f6d7cd5c12cbbf454902a6005cbfb2
Akash M <akashm1999@gmail.com>
2020-06-26 20:44:58 +0530

    Added Manage User Modal

--- Angular2/src/app/pages/users/users.module.ts
+++ Angular2/src/app/pages/users/users.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UIModule } from '../../shared/ui/ui.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { UsersRoutingModule } from './users-routing.module';
 import { NgSelectModule } from '@ng-select/ng-select';
@@ -15,6 +16,8 @@ import { Ng2SearchPipeModule } from 'ng2-search-filter';
     UsersRoutingModule,
     Ng2SearchPipeModule,
     NgSelectModule,
+    FormsModule,
+    ReactiveFormsModule,
     UIModule
   ]
 })
