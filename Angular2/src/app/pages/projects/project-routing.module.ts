37bff4dc143cfe4189ca78f585fa098e91fd0023
Akash M <akashm1999@gmail.com>
2020-06-27 17:26:11 +0530

    Added Project View Routing

--- Angular2/src/app/pages/projects/project-routing.module.ts
+++ Angular2/src/app/pages/projects/project-routing.module.ts
@@ -2,8 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import {ProjectManageComponent} from './manage/manage.component';
+import {ProjectViewComponent} from './view/view.component';
 
-const routes: Routes = [ {path: 'manage', component: ProjectManageComponent}];
+const routes: Routes = [ 
+    {path: 'manage', component: ProjectManageComponent},
+    {path: 'view', component: ProjectViewComponent},
+];
 
 @NgModule({
     imports: [RouterModule.forChild(routes)],
