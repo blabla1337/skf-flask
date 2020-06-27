e1fd5cefb91985a8beca248d745fa48e8cc6a702
Akash M <38563661+AkashM398@users.noreply.github.com>
2020-06-27 14:59:13 +0530

    Added Modal at Manage User & Code Example (#620)
    * Added Button, Updated alignments
    
    * Added Card Contents
    
    * Added helper classes and removed unwanted code
    
    * Added CSS to User Page
    
    * Completed Users Page
    
    * Changes in CSS
    
    * Created Code Example Page
    
    * added code routing
    
    * Fixed Code Example Routing
    
    * Completed the UI for Code Examples
    
    * Knowledgebase Created & Added Routing
    
    * fixed collapse
    
    * Modifies Knowledgebase files
    
    * implemented knowledgebase design
    
    * created checklists
    
    * Added Routing
    
    * implementing dynamic theme
    
    * Implented Checklist View
    
    * removed unwanted CSS and fixed Checklists View
    
    * Created Projects Page
    
    * Implemented Project Page
    
    * Added Login Page
    
    * Added Login Routing
    
    * completed login page and fixed layout
    
    * Added Manage User Modal
    
    * Added Code Example Modal
    
    * Updated User Modal
    
    * Modified Angular json configuration

--- Angular2/src/app/pages/code-example/view/view.component.ts
+++ Angular2/src/app/pages/code-example/view/view.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 import { Codes } from './code-example.model';
 
@@ -16,11 +18,10 @@ export class CodeViewComponent implements OnInit {
 
   codeData: Codes[];
 
-
   // Collapse declare
   isCollapsed: boolean;
 
-  constructor() { }
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit() {
     this.breadCrumbItems = [{ label: 'Code Examples' }, { label: 'View', active: true }];
@@ -37,4 +38,13 @@ export class CodeViewComponent implements OnInit {
   private _fetchData() {
     this.codeData = codeData;
   }
+
+  /**
+   * Open center modal
+   * @param centerDataModal center modal data
+   */
+  centerModal(centerDataModal: any) {
+    this.modalService.open(centerDataModal, { centered: true });
+  }
+
 }
