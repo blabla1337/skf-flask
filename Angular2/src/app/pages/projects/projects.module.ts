2d978ee96fc6f4ab3fdf5578ea08a065f522eb33
Akash M <akashm1999@gmail.com>
2020-06-27 17:20:14 +0530

    Added Project View

--- Angular2/src/app/pages/projects/projects.module.ts
+++ Angular2/src/app/pages/projects/projects.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProjectRoutingModule } from './project-routing.module';
+import { ViewComponent } from './view/view.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [ViewComponent],
   imports: [
     CommonModule,
     ProjectRoutingModule
