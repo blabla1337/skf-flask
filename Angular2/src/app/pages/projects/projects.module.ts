48ace2aa3f3f27b3949f065952282b515c38e521
Akash M <38563661+AkashM398@users.noreply.github.com>
2020-06-27 23:57:57 +0530

    Added Project View Page & Fixed Configuration Modal (#621)
    * Added Button, Updated alignments
    
    * Added Card Contents
    
    * Added helper classes and removed unwanted code
    
    * Added CSS to User Page
    
    * Completed Users Page
    
    * Changes in CSS
    
    * Created Code Example Page
    
    * added code routing
    
    * Fixed Code Example Routing
    
    * Completed the UI for Code Examples
    
    * Knowledgebase Created & Added Routing
    
    * fixed collapse
    
    * Modifies Knowledgebase files
    
    * implemented knowledgebase design
    
    * created checklists
    
    * Added Routing
    
    * implementing dynamic theme
    
    * Implented Checklist View
    
    * removed unwanted CSS and fixed Checklists View
    
    * Created Projects Page
    
    * Implemented Project Page
    
    * Added Login Page
    
    * Added Login Routing
    
    * completed login page and fixed layout
    
    * Added Manage User Modal
    
    * Added Code Example Modal
    
    * Updated User Modal
    
    * Modified Angular json configuration
    
    * Added CSS for Projects
    
    * Added Project View
    
    * Added Project View Routing
    
    * Added Project View Modal
    
    * fixed project view modal

--- Angular2/src/app/pages/projects/projects.module.ts
+++ Angular2/src/app/pages/projects/projects.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ArchwizardModule } from 'angular-archwizard';
+
 import { ProjectRoutingModule } from './project-routing.module';
+
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ArchwizardModule,
     ProjectRoutingModule
   ]
 })
