db116787d9b577f225a025dd5eeedd3773a09a5e
Akash M <akashm1999@gmail.com>
2020-06-28 21:00:30 +0530

    Added Checklists Read Page

---
+++ Angular2/src/app/pages/checklists/read/read.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-read',
+  templateUrl: './read.component.html',
+  styleUrls: ['./read.component.scss']
+})
+export class ReadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
