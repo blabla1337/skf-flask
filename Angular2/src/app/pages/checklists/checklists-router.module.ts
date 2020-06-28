a45c9a26603b3b20bc742b7ee46a7595ebd4fab0
Akash M <akashm1999@gmail.com>
2020-06-28 22:26:14 +0530

    Implemented Checklist Knowledgebase

--- Angular2/src/app/pages/checklists/checklists-router.module.ts
+++ Angular2/src/app/pages/checklists/checklists-router.module.ts
@@ -2,8 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import {ViewComponent} from './view/view.component';
+import { ChecklistsReadComponent } from './read/read.component';
 
-const routes: Routes = [ {path: 'view', component: ViewComponent}];
+const routes: Routes = [
+    {path: 'view', component: ViewComponent},
+    {path: 'read', component: ChecklistsReadComponent}
+];
 
 @NgModule({
     imports: [RouterModule.forChild(routes)],
