fe2b508c8bbe281a01c56a8602014afa0991c4f1
Akash M <akashm1999@gmail.com>
2020-06-28 23:16:24 +0530

    Added Router Links

--- Angular2/src/app/layouts/horizontaltopbar/menu.ts
+++ Angular2/src/app/layouts/horizontaltopbar/menu.ts
@@ -5,27 +5,31 @@ export const MENU: MenuItem[] = [
         id: 1,
         label: 'Dashboards',
         icon: 'mdi mdi-18px mdi-view-dashboard mr-2',
+        link: '/dashboard',
     },
     {
         id: 2,
         label: 'Manage Projects',
         icon: 'mdi mdi-18px mdi-card-bulleted mr-2',
-        isUiElement: true,
+        link: '/projects/manage',
     },
     {
         id: 3,
         icon: 'mdi mdi-18px mdi-account-multiple mr-2',
         label: 'Users',
+        link: 'users/manage'
     },
     {
         id: 4,
         icon: 'mdi mdi-18px mdi-text-box-check mr-2',
         label: 'Checklists',
+        link: 'checklists/view',
     },
     {
         id: 5,
         label: 'Code examples',
         icon: 'mdi mdi-18px mdi-code-greater-than-or-equal mr-2',
+        link: 'code-example/view'
     }
 ];
 
