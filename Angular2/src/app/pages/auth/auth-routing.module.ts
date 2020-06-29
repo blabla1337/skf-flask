7105da59e8b0f2466185107f05c59a9d5beedca3
Akash M <akashm1999@gmail.com>
2020-06-29 21:50:58 +0530

    Implemented Register Page

--- Angular2/src/app/pages/auth/auth-routing.module.ts
+++ Angular2/src/app/pages/auth/auth-routing.module.ts
@@ -2,8 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [{path: 'login', component: LoginComponent}];
+const routes: Routes = [
+    {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent},
+];
 
 @NgModule({
     imports: [RouterModule.forChild(routes)],
