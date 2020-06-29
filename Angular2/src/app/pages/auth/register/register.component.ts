22ad18741502d9ab7f49092475494593784d6fa4
Akash M <akashm1999@gmail.com>
2020-06-29 21:50:58 +0530

    Implemented Register Page

--- Angular2/src/app/pages/auth/register/register.component.ts
+++ Angular2/src/app/pages/auth/register/register.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  year: number = new Date().getFullYear();
+
   constructor() { }
 
   ngOnInit(): void {
