d4ea79138ed3646e13a2aaf547d320be9ed1f130
Akash M <akashm1999@gmail.com>
2020-07-01 23:49:13 +0530

    Fixed Knowledgebase Collapse and Added Modals

---
+++ Angular2/src/app/pages/knowledgebase/data.ts
@@ -0,0 +1,58 @@
+const knowledgeData = [
+    {
+        id: 1,
+        platform: 'Web Application',
+        subData: [
+            {
+                id: 1,
+                title: 'Title1',
+                content: 'Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente eaproident.',
+                description: 'This is a sample Description',
+                platform: 'web',
+            },
+            {
+                id: 2,
+                title: 'Title2',
+                content: 'Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente eaproident.',
+                description: 'This is a sample Description',
+                platform: 'web',
+            },
+            {
+                id: 3,
+                title: 'Title3',
+                content: 'Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente eaproident.',
+                description: 'This is a sample Description',
+                platform: 'web',
+            },
+        ]
+    },
+    {
+        id: 1,
+        platform: 'Mobile Application',
+        subData: [
+            {
+                id: 1,
+                title: 'Title1',
+                content: 'Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente eaproident.',
+                description: 'This is a sample Description',
+                platform: 'mobile',
+            },
+            {
+                id: 2,
+                title: 'Title2',
+                content: 'Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente eaproident.',
+                description: 'This is a sample Description',
+                platform: 'mobile',
+            },
+            {
+                id: 3,
+                title: 'Title3',
+                content: 'Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente eaproident.',
+                description: 'This is a sample Description',
+                platform: 'mobile',
+            },
+        ]
+    },
+];
+
+export { knowledgeData };
---
+++ Angular2/src/app/pages/knowledgebase/knowledgebase.model.ts
@@ -0,0 +1,10 @@
+export interface Knowledgebase {
+    id?: number;
+    title?: string;
+    content?: string;
+    description?: string;
+    solution?: string;
+    text?: string;
+    subtext?: string;
+    platform?: string;
+}
