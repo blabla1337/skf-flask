d7751503282af731c346b29cd015afa9ebebf2be
Akash M <akashm1999@gmail.com>
2020-07-08 14:33:51 +0530

    Fixed Knowledgebase

--- Angular2/src/app/pages/auth/login/login.component.ts
+++ Angular2/src/app/pages/auth/login/login.component.ts
@@ -1,17 +1,57 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
 
-@Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
-})
+import { AuthenticateService} from '../../../core/services/auth.service';
+
+@Component({ templateUrl: 'login.component.html' })
 export class LoginComponent implements OnInit {
+    loginForm: FormGroup;
+    loading = false;
+    submitted = false;
+    returnUrl: string;
 
-  year: number = new Date().getFullYear();
+    constructor(
+        private formBuilder: FormBuilder,
+        private route: ActivatedRoute,
+        private router: Router,
+        private accountService: AuthenticateService,
+    ) {
+        // redirect to home if already logged in
+        if (this.accountService.userValue) {
+            this.router.navigate(['/']);
+        }
+    }
 
-  constructor() { }
+    ngOnInit() {
+        this.loginForm = this.formBuilder.group({
+            username: ['', Validators.required],
+            password: ['', Validators.required]
+        });
 
-  ngOnInit(): void {
-  }
+        // get return url from route parameters or default to '/'
+        this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+    }
 
+    // convenience getter for easy access to form fields
+    get f() { return this.loginForm.controls; }
+
+    onSubmit() {
+        this.submitted = true;
+
+        // stop here if form is invalid
+        if (this.loginForm.invalid) {
+            return;
+        }
+
+        this.loading = true;
+        this.accountService.authenticate(this.f.username.value, this.f.password.value)
+            .pipe(first())
+            .subscribe(
+                data => {
+                    this.router.navigate([this.returnUrl]);
+                },
+               );
+    }
 }
--- Angular2/src/app/pages/knowledgebase/data.ts
+++ Angular2/src/app/pages/knowledgebase/data.ts
@@ -27,12 +27,39 @@ const knowledgeData = [
         ]
     },
     {
-        id: 1,
+        id: 2,
         platform: 'Mobile Application',
         subData: [
             {
                 id: 1,
-                title: 'Title1',
+                title: 'Title1r',
+                content: 'Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente eaproident.',
+                description: 'This is a sample Description',
+                platform: 'mobile',
+            },
+            {
+                id: 2,
+                title: 'Title2',
+                content: 'Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente eaproident.',
+                description: 'This is a sample Description',
+                platform: 'mobile',
+            },
+            {
+                id: 3,
+                title: 'Title3',
+                content: 'Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente eaproident.',
+                description: 'This is a sample Description',
+                platform: 'mobile',
+            },
+        ]
+    },
+    {
+        id: 3,
+        platform: 'IOT Application',
+        subData: [
+            {
+                id: 1,
+                title: 'Title1wdwe',
                 content: 'Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente eaproident.',
                 description: 'This is a sample Description',
                 platform: 'mobile',
