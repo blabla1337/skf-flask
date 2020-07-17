96761af577c36294a3fa02b794f30200fd435b96
Akash M <akashm1999@gmail.com>
2020-07-17 23:48:53 +0530

    Added Nav menu in Checklist manage

--- Angular2/src/app/pages/checklists/checklists.module.ts
+++ Angular2/src/app/pages/checklists/checklists.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 import { ChecklistsRoutingModule } from './checklists-router.module';
 
 
