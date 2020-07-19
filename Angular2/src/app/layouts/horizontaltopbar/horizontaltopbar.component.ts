9ebb72e84827381f2e254bb713b686926c2fce63
Akash M <38563661+AkashM398@users.noreply.github.com>
2020-07-20 03:03:33 +0530

    Implemented Dynamic Theme (#637)
    * Added Button, Updated alignments
    
    * Added Card Contents
    
    * Added helper classes and removed unwanted code
    
    * Added CSS to User Page
    
    * Completed Users Page
    
    * Changes in CSS
    
    * Created Code Example Page
    
    * added code routing
    
    * Fixed Code Example Routing
    
    * Completed the UI for Code Examples
    
    * Knowledgebase Created & Added Routing
    
    * fixed collapse
    
    * Modifies Knowledgebase files
    
    * implemented knowledgebase design
    
    * created checklists
    
    * Added Routing
    
    * implementing dynamic theme
    
    * Implented Checklist View
    
    * removed unwanted CSS and fixed Checklists View
    
    * Created Projects Page
    
    * Implemented Project Page
    
    * Added Login Page
    
    * Added Login Routing
    
    * completed login page and fixed layout
    
    * Added Manage User Modal
    
    * Added Code Example Modal
    
    * Updated User Modal
    
    * Modified Angular json configuration
    
    * Added CSS for Projects
    
    * Added Project View
    
    * Added Project View Routing
    
    * Added Project View Modal
    
    * fixed project view modal
    
    * Made some UI enhancement
    
    * Enhanced Knowledgebase UI
    
    * Added Checklists Read Page
    
    * Implemented Checklist Knowledgebase
    
    * Added Platform Options
    
    * Added Router Links
    
    * Enhanced the UI
    
    * improved Login Page
    
    * Created Register Page
    
    * Implemented Register Page
    
    * Created Lab View Pageg
    
    * Added Lab View Routing
    
    * Added Labs Read Page
    
    * Added Labs Read Routing
    
    * Implemented Lab Read Page
    
    * Removed Share Folder
    
    * Made some Design tweaks
    
    * Added Some funtionalities to UI
    
    * Fixed Collapse Issue
    
    * Changes Dashboard icons
    
    * Fixed Collapse in Checklists
    
    * Fixed Knowledgebase Collapse and Added Modals
    
    * Completed Lab Page
    
    * Fixed Knowledgebase
    
    * Fixed Knowledge base UI and Fixed Login Page
    
    * Added Footer
    
    * fixed users manage page
    
    * Fixed Routing Links
    
    * Completed Signup Functionality
    
    * Fixed Login Page and Added LocalStorage Functionality
    
    * removed unwanted code
    
    * Created OpenId Page
    
    * Removed unwanted code
    
    * Added Code Example Edit Page
    
    * Updated id names
    
    * Fixed Project Wizard Menu
    
    * Added Nav menu in Checklist manage
    
    * Completed Checklist Item
    
    * Completed Project Summary Page
    
    * Implemented Dynamic Themes
    
    * Added Inline Comments

--- Angular2/src/app/layouts/horizontaltopbar/horizontaltopbar.component.ts
+++ Angular2/src/app/layouts/horizontaltopbar/horizontaltopbar.component.ts
@@ -22,7 +22,8 @@ export class HorizontaltopbarComponent implements OnInit, AfterViewInit {
 
   loggedinUser: string;
   loggedin = false;
-
+  dark = false;
+  light = true;
   menuItems = [];
 
   // tslint:disable-next-line: max-line-length
@@ -81,6 +82,42 @@ export class HorizontaltopbarComponent implements OnInit, AfterViewInit {
   toggleMenubar() {
     const element = document.getElementById('topnav-menu-content');
     element.classList.toggle('show');
+  }
+
+  /**
+   * Change to Dark theme
+   */
+  toDark(theme: string) {
+    this.changeTheme(theme);
+    this.light = false;
+    this.dark = true;
+  }
+
+  /**
+   * Change to Light theme
+   */
+  toLight(theme: string) {
+    this.changeTheme(theme);
+    this.dark = false;
+    this.light = true;
+  }
+
+  /**
+   * Dynamic Theme
+   */
+  changeTheme(styleName: string) {
+    const head = this.document.getElementsByTagName('head')[0];
+    const themeLink = this.document.getElementById('dynamic-theme') as HTMLLinkElement;
+    if (themeLink) {
+      themeLink.href = styleName;
+    } else {
+      const style = this.document.createElement('link');
+      style.id = 'dynamic-theme';
+      style.rel = 'stylesheet';
+      style.href = `${styleName}`;
+
+      head.appendChild(style);
+    }
   }
 
   /**
