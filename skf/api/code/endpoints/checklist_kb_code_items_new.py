d97b4be57331c8f8bc9179b996e76f1a4f06fd46
Riiecco <r.tencate77@gmail.com>
2020-07-20 10:56:46 +0200

    checklist kb to code example correlation

---
+++ skf/api/code/endpoints/checklist_kb_code_items_new.py
@@ -0,0 +1,25 @@
+from flask import request
+from flask_restplus import Resource
+from skf.api.security import security_headers
+from skf.api.code.business import create_code_item_checklist_kb
+from skf.api.code.serializers import code_items_checklist_kb_all, message
+from skf.api.restplus import api
+from skf.api.security import log, val_num, val_alpha, val_alpha_num, val_alpha_num_special
+
+ns = api.namespace('code', description='Operations related to code example items')
+
+@ns.route('/items/requirements/new/<int:checklist_kb_id>/<int:code_id>')
+@api.response(404, 'Validation error', message)
+class CodeCollection(Resource):
+
+    #@api.marshal_with(code_items_checklist_kb_all)
+    @api.response(400, 'No results found', message)
+    def put(self, checklist_kb_id, code_id):
+        """
+        Correlate code example item to requirements
+        * Privileges required: **none**
+        """
+        val_num(checklist_kb_id)
+        val_num(code_id)
+        result = create_code_item_checklist_kb(checklist_kb_id, code_id)
+        return result, 200, security_headers()
