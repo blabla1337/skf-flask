5eee8626073018cf17ef7cfe4f6f82e0bedc7d91
Akash M <akashm1999@gmail.com>
2020-07-21 19:29:57 +0530

    Added Code Example Create Page

--- Angular2/src/app/pages/code-example/code-example.module.ts
+++ Angular2/src/app/pages/code-example/code-example.module.ts
@@ -6,11 +6,12 @@ import { CodeExampleRoutingModule } from './code-example-routing.module';
 import { NgSelectModule } from '@ng-select/ng-select';
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
 import { EditComponent } from './edit/edit.component';
+import { CodeCreateComponent } from './create/create.component';
 
 
 
 @NgModule({
-  declarations: [EditComponent],
+  declarations: [EditComponent, CodeCreateComponent],
   imports: [
     CommonModule,
     CodeExampleRoutingModule,
