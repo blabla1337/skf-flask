eaa7ed5eae3781b39849bc03e6ceacdc733b46c7
Akash M <akashm1999@gmail.com>
2020-07-21 19:29:57 +0530

    Added Code Example Create Page

--- Angular2/src/app/pages/code-example/code-example-routing.module.ts
+++ Angular2/src/app/pages/code-example/code-example-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 import {CodeViewComponent} from './view/view.component';
 import {EditComponent} from './edit/edit.component';
+import { CodeCreateComponent } from './create/create.component';
 
 const routes: Routes = [
     {path: 'view', component: CodeViewComponent},
     {path: 'edit', component: EditComponent},
+    {path: 'create', component: CodeCreateComponent},
 ];
 
 @NgModule({
