7f627d728b83f28396d48a1a5b86089e963f9c7b
Riiecco <r.tencate77@gmail.com>
2020-07-23 14:26:29 +0200

    GET projects - create new projects

---
+++ Angular2/src/app/core/services/project.service.ts
@@ -0,0 +1,37 @@
+
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { Observable } from 'rxjs';
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class ProjectService
+{
+    public category_id: number;
+
+    constructor(
+        private http: HttpClient,
+    ) { }
+
+    public authHeader = new HttpHeaders({ 'Content-Type': 'application/json', 'Authorization': sessionStorage.getItem("Authorization") });
+
+    getProjectsCollection(): Observable<Object>
+    {
+        return this.http.get(environment.API_ENDPOINT + `/api/project/items`, { headers: this.authHeader })
+    }
+
+    createProject(value: any): Observable<Object>
+    {
+        return this.http.put(environment.API_ENDPOINT + `/api/project/new`, value, { headers: this.authHeader })
+    }
+
+
+    deleteProject(id: number): Observable<Object>
+    {
+        return this.http.delete(environment.API_ENDPOINT + `/api/project/delete/${id}`, { headers: this.authHeader })
+    }
+
+}
