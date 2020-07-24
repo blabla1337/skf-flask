e0ee4b3ed0c7ec6fc100c64a9d4c20fde36b7252
Riiecco <r.tencate77@gmail.com>
2020-07-24 10:46:10 +0200

    Get single project value from skf API

---
+++ skf/api/projects/endpoints/project_item.py
@@ -0,0 +1,30 @@
+
+from flask import request
+from flask_restplus import Resource
+from skf.api.security import security_headers, validate_privilege, select_userid_jwt
+from skf.api.projects.business import get_project_item
+from skf.api.projects.serializers import project_stats, message
+from skf.api.projects.parsers import authorization
+from skf.api.restplus import api
+from skf.api.security import log, val_num, val_alpha, val_alpha_num, val_alpha_num_special
+
+ns = api.namespace('project', description='Operations related to kb items')
+
+@ns.route('/item/<int:id>')
+@api.doc(params={'id': 'The project item id'})
+@api.response(404, 'Validation error', message)
+class Project(Resource):
+
+    @api.expect(authorization, get_project_item)
+    @api.marshal_with(project_stats)
+    @api.response(400, 'No results found', message)
+    def get(self, id):
+        """
+        Create new project item.
+        * Privileges required: **edit**
+        """
+        val_num(id)
+        result = get_project_item(id)
+        validate_privilege(self, 'edit')
+        return result, 200, security_headers()
+
