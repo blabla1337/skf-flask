da96ae535bc39a8ad81eb3645919f9db8c3c1695
Glenn ten Cate <glenntencate@gmail.com>
2020-07-27 20:13:07 +0200

    Added search labs

---
+++ Angular2/src/app/core/pipes/labelfilter.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { LabelFilterPipe } from './labelfilter.pipe';
+
+describe('LabelFilterPipe', () => {
+  it('create an instance', () => {
+    const pipe = new LabelFilterPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
---
+++ Angular2/src/app/core/pipes/labelfilter.pipe.ts
@@ -0,0 +1,19 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+    name: 'labelfilter'
+})
+export class LabelFilterPipe implements PipeTransform
+{
+
+    transform(items: any[], args: any[]): any
+    {
+        if (args && args.length > 0) {
+            return items.filter(item => args.indexOf(item.label) >= 0);
+        }
+        else {
+            return items;
+        }
+    }
+
+}
