378cb1c1429249cb8e959e5d717e4c93f883a625
Glenn ten Cate <glenntencate@gmail.com>
2020-07-28 10:36:41 +0200

    lab service + api modification

--- Angular2/src/app/core/services/lab.service.ts
+++ Angular2/src/app/core/services/lab.service.ts
@@ -1,6 +1,6 @@
 
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 
 @Injectable({
@@ -14,9 +14,20 @@ export class LabService
         private http: HttpClient,
     ) { }
 
+    public authHeader = new HttpHeaders({ 'Content-Type': 'application/json', 'Authorization': sessionStorage.getItem("Authorization") });
+
     getLabs()
     {
         return this.http.get(environment.API_ENDPOINT + '/api/interactive_labs/items')
     }
 
+    deployLab(image_tag)
+    {
+      return this.http.get(environment.API_ENDPOINT + `/interactive_labs/deployments/${image_tag}`, { headers: this.authHeader })
+    }
+  
+    deleteLab(image_tag)
+    {
+      return this.http.get(environment.API_ENDPOINT + `/interactive_labs/delete-deployments/${image_tag}`, { headers: this.authHeader })
+    }
 }
