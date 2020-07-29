8f8710ac22d2383d19e037ea440128975429f5f2
Riiecco <r.tencate77@gmail.com>
2020-07-29 13:15:46 +0200

    asdsa

--- Angular2/src/app/core/services/sprint.service.ts
+++ Angular2/src/app/core/services/sprint.service.ts
@@ -28,27 +28,17 @@ export class SprintService
         return this.http.get(environment.API_ENDPOINT + `/api/sprint/results/${id}`, { headers: this.authHeader })
     }
 
-    getSprintItem(id: number): Observable<Object>
-    {
-        return this.http.get(environment.API_ENDPOINT + `/api/project/item/${id}`, { headers: this.authHeader })
-    }
-
     createSprint(value: any): Observable<Object>
     {
-        return this.http.put(environment.API_ENDPOINT + `/api/project/new`, value, { headers: this.authHeader })
+        return this.http.put(environment.API_ENDPOINT + `/api/sprint/new`, value, { headers: this.authHeader })
     }
 
-    updateSprint(id: number, value: any): Observable<Object>
-    {
-        return this.http.put(environment.API_ENDPOINT + `/api/project/update/${id}`, value, { headers: this.authHeader })
-    }
-
-    deleteChecklistResultFromSprint(id: number): Observable<Object>
+    deleteSprint(id: number): Observable<Object>
     {
         return this.http.delete(environment.API_ENDPOINT + `/api/project/delete/${id}`, { headers: this.authHeader })
     }
 
-    deleteSprint(id: number): Observable<Object>
+    deleteChecklistResultFromSprint(id: number): Observable<Object>
     {
         return this.http.delete(environment.API_ENDPOINT + `/api/project/delete/${id}`, { headers: this.authHeader })
     }
