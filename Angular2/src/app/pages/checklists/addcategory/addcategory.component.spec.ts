2efddc6b4b7057e842fe840bdbf97316c2183320
Akash M <38563661+AkashM398@users.noreply.github.com>
2020-07-30 22:35:44 +0530

    Added Category Add and Update Pages (#666)
    * Removed unwanted code
    
    * Modified Code Example UI
    
    * Added Code Creation Button
    
    * Added Code Example Create Page
    
    * Added Delete Modals
    
    * Added Loader and Knowledgebase create button
    
    * removed loader code
    
    * modified knowledgebase
    
    * Added Icon Plugin
    
    * Added Project Wizard Page
    
    * Modified Checklists Manage Page
    
    * Fixed an error
    
    * Fixed Project Summary Dropdown
    
    * Fixed Project Summary Accordian
    
    * Completed Category Page
    
    * Modified Lab Option Menu
    
    * Added Forms in Wizard
    
    * Added Dropdown Menu
    
    * Removed Unwanted Code and Tweaked Design
    
    * Fixed Merge Conflict
    
    * Fixed Merge Conflict
    
    * Added User Add and Update Page
    
    * Added Checklists Create View
    
    * Made few changes
    
    * Made minor changes
    
    * Added Add and Update Checklist Page
    
    * Added Add and Update Category Pages
    
    * made minor changes

---
+++ Angular2/src/app/pages/checklists/addcategory/addcategory.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AddCategoryComponent } from './addcategory.component';
+
+describe('AddCategoryComponent', () => {
+  let component: AddCategoryComponent;
+  let fixture: ComponentFixture<AddCategoryComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ AddCategoryComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AddCategoryComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
