45b4148ac3dabfa832440890ca41dd4e4cc0b023
Akash M <38563661+AkashM398@users.noreply.github.com>
2020-07-30 21:06:08 +0530

    Completed Add and Update Checklists Item Pages (#665)
    * Removed unwanted code
    
    * Modified Code Example UI
    
    * Added Code Creation Button
    
    * Added Code Example Create Page
    
    * Added Delete Modals
    
    * Added Loader and Knowledgebase create button
    
    * removed loader code
    
    * modified knowledgebase
    
    * Added Icon Plugin
    
    * Added Project Wizard Page
    
    * Modified Checklists Manage Page
    
    * Fixed an error
    
    * Fixed Project Summary Dropdown
    
    * Fixed Project Summary Accordian
    
    * Completed Category Page
    
    * Modified Lab Option Menu
    
    * Added Forms in Wizard
    
    * Added Dropdown Menu
    
    * Removed Unwanted Code and Tweaked Design
    
    * Fixed Merge Conflict
    
    * Fixed Merge Conflict
    
    * Added User Add and Update Page
    
    * Added Checklists Create View
    
    * Made few changes
    
    * Made minor changes
    
    * Added Add and Update Checklist Page

---
+++ Angular2/src/app/pages/checklists/addchecklist/addchecklist.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AddChecklistComponent } from './addchecklist.component';
+
+describe('AddChecklistComponent', () => {
+  let component: AddChecklistComponent;
+  let fixture: ComponentFixture<AddChecklistComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ AddChecklistComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AddChecklistComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
