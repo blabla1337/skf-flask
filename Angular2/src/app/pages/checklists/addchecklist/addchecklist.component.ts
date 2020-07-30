f32afc0ff12cebc7c54244e90620342935745dc5
Akash M <akashm1999@gmail.com>
2020-07-30 21:00:50 +0530

    Added Add and Update Checklist Page

---
+++ Angular2/src/app/pages/checklists/addchecklist/addchecklist.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-addchecklist',
+  templateUrl: './addchecklist.component.html',
+  styleUrls: ['./addchecklist.component.scss']
+})
+export class AddChecklistComponent implements OnInit {
+
+  // bread crumb items
+  breadCrumbItems: Array<{}>;
+
+  // Form Validation
+  validationform: FormGroup;
+  typeValidationForm: FormGroup;
+
+  // Form Submission
+  submit: boolean;
+
+  kbitem: string[];
+
+  constructor( private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.breadCrumbItems = [{ label: 'Users' }, { label: 'Create', active: true }];
+
+    this.kbitem = ['knowledgebase-1', 'knowledgebase-2', 'knowledgebase-3', 'knowledgebase-4', 'knowledgebase-5', 'knowledgebase-6'];
+
+    /**
+     * Bootstrap validation form data
+     */
+    this.validationform = this.formBuilder.group({
+      checkid: ['', [Validators.required, Validators.pattern('[0-9]._-+')]],
+      checktitle: ['', [Validators.required, Validators.pattern('[a-zA-Z0-9]+')]],
+      maturitylvl: ['', [Validators.required, Validators.pattern('[0-9]._-+')]],
+      cwe: ['', [Validators.required, Validators.pattern('[0-9]._-+')]],
+    });
+    this.submit = false;
+  }
+
+  /**
+   * Returns form
+   */
+  get form() {
+    return this.validationform.controls;
+  }
+
+  /**
+   * Validation form submit method
+   */
+  validSubmit() {
+    this.submit = true;
+  }
+
+}
---
+++ Angular2/src/app/pages/checklists/updatechecklist/updatechecklist.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-updatechecklist',
+  templateUrl: './updatechecklist.component.html',
+  styleUrls: ['./updatechecklist.component.scss']
+})
+export class UpdateChecklistComponent implements OnInit {
+
+  // bread crumb items
+  breadCrumbItems: Array<{}>;
+
+  // Form Validation
+  validationform: FormGroup;
+  typeValidationForm: FormGroup;
+
+  // Form Submission
+  submit: boolean;
+
+  kbitem: string[];
+
+  constructor( private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.breadCrumbItems = [{ label: 'Users' }, { label: 'Update', active: true }];
+
+    this.kbitem = ['Select here', 'knowledgebase-1', 'knowledgebase-2', 'knowledgebase-3', 'knowledgebase-4', 'knowledgebase-5', 'knowledgebase-6'];
+
+    /**
+     * Bootstrap validation form data
+     */
+    this.validationform = this.formBuilder.group({
+      checkid: ['', [Validators.required, Validators.pattern('[0-9]._-+')]],
+      checktitle: ['', [Validators.required, Validators.pattern('[a-zA-Z0-9]+')]],
+      maturitylvl: ['', [Validators.required, Validators.pattern('[0-9]._-+')]],
+      cwe: ['', [Validators.required, Validators.pattern('[0-9]._-+')]],
+    });
+    this.submit = false;
+  }
+
+  /**
+   * Returns form
+   */
+  get form() {
+    return this.validationform.controls;
+  }
+
+  /**
+   * Validation form submit method
+   */
+  validSubmit() {
+    this.submit = true;
+  }
+
+}
