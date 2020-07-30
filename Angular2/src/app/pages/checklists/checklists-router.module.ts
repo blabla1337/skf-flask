9a2c7d51679564dec22f5223210cd261907821a9
Akash M <38563661+AkashM398@users.noreply.github.com>
2020-07-30 21:06:08 +0530

    Completed Add and Update Checklists Item Pages (#665)
    * Removed unwanted code
    
    * Modified Code Example UI
    
    * Added Code Creation Button
    
    * Added Code Example Create Page
    
    * Added Delete Modals
    
    * Added Loader and Knowledgebase create button
    
    * removed loader code
    
    * modified knowledgebase
    
    * Added Icon Plugin
    
    * Added Project Wizard Page
    
    * Modified Checklists Manage Page
    
    * Fixed an error
    
    * Fixed Project Summary Dropdown
    
    * Fixed Project Summary Accordian
    
    * Completed Category Page
    
    * Modified Lab Option Menu
    
    * Added Forms in Wizard
    
    * Added Dropdown Menu
    
    * Removed Unwanted Code and Tweaked Design
    
    * Fixed Merge Conflict
    
    * Fixed Merge Conflict
    
    * Added User Add and Update Page
    
    * Added Checklists Create View
    
    * Made few changes
    
    * Made minor changes
    
    * Added Add and Update Checklist Page

--- Angular2/src/app/pages/checklists/checklists-router.module.ts
+++ Angular2/src/app/pages/checklists/checklists-router.module.ts
@@ -5,12 +5,16 @@ import { ViewComponent } from './view/view.component';
 import { ChecklistsReadComponent } from './read/read.component';
 import { CheckManageComponent } from './manage/manage.component';
 import { ChecklistCreateComponent } from './create/create.component';
+import { AddChecklistComponent } from './addchecklist/addchecklist.component';
+import { UpdateChecklistComponent } from './updatechecklist/updatechecklist.component';
 
 const routes: Routes = [
     {path: 'view', component: ViewComponent},
     {path: 'read', component: ChecklistsReadComponent},
     {path: 'manage', component: CheckManageComponent},
     {path: 'create', component: ChecklistCreateComponent},
+    {path: 'add', component: AddChecklistComponent},
+    {path: 'update', component: UpdateChecklistComponent},
 ];
 
 @NgModule({
