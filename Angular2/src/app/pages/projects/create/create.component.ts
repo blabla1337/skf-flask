8db776d9e155e9aa8ecbe013dfa9059ef18ba9fd
Glenn ten Cate <glenntencate@gmail.com>
2020-07-31 23:04:59 +0200

    added validation for project create and update

--- Angular2/src/app/pages/projects/create/create.component.ts
+++ Angular2/src/app/pages/projects/create/create.component.ts
@@ -12,36 +12,54 @@ import { ProjectService } from '../../../core/services/project.service';
 })
 export class ProjectCreateComponent implements OnInit
 {
-
   // bread crumb items
   breadCrumbItems: Array<{}>;
   public projectForm: FormGroup;
-  public isSubmitted: boolean;
+
+  // Form Submission
+  public submit: boolean;
+  public formsubmit: boolean;
 
   constructor(
     private formBuilder: FormBuilder,
     private router: Router,
-    private _projectService: ProjectService) { }
+    private _projectService: ProjectService
+  ) { }
 
   ngOnInit(): void
   {
     this.breadCrumbItems = [{ label: 'Projects' }, { label: 'Create', active: true }];
 
     this.projectForm = this.formBuilder.group({
-      name: ['', Validators.required],
-      description: ['', Validators.required],
-      version: ['', Validators.required],
-    })
-
+      name: ['', [Validators.required, Validators.pattern('[a-zA-Z0-9]+')]],
+      description: ['', [Validators.required, Validators.pattern('[a-zA-Z0-9]+')]],
+      version: ['', [Validators.required, Validators.pattern('[a-zA-Z0-9.]+')]],
+    });
+    this.submit = false;
   }
 
   createProject()
   {
-    this.isSubmitted = true;
+    this.submit = true;
     if (this.projectForm.invalid) {
       return;
     }
     this._projectService.createProject(this.projectForm.value).subscribe()
     this.router.navigate(['/projects/manage'])
   }
+
+  /**
+   * Returns form
+   */
+  get form() {
+    return this.projectForm.controls;
+  }
+
+  /**
+   * Validation form submit method
+   */
+  validSubmit() {
+    this.submit = true;
+  }
+
 }
