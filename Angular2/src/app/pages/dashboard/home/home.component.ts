0ac02e0784c823664cf275688a55da5675978049
Akash M <38563661+AkashM398@users.noreply.github.com>
2020-07-31 20:38:23 +0530

    Added Tour (#668)
    * Removed unwanted code
    
    * Modified Code Example UI
    
    * Added Code Creation Button
    
    * Added Code Example Create Page
    
    * Added Delete Modals
    
    * Added Loader and Knowledgebase create button
    
    * removed loader code
    
    * modified knowledgebase
    
    * Added Icon Plugin
    
    * Added Project Wizard Page
    
    * Modified Checklists Manage Page
    
    * Fixed an error
    
    * Fixed Project Summary Dropdown
    
    * Fixed Project Summary Accordian
    
    * Completed Category Page
    
    * Modified Lab Option Menu
    
    * Added Forms in Wizard
    
    * Added Dropdown Menu
    
    * Removed Unwanted Code and Tweaked Design
    
    * Fixed Merge Conflict
    
    * Fixed Merge Conflict
    
    * Added User Add and Update Page
    
    * Added Checklists Create View
    
    * Made few changes
    
    * Made minor changes
    
    * Added Add and Update Checklist Page
    
    * Added Add and Update Category Pages
    
    * made minor changes
    
    * Implemented Load Spinner
    
    * Added Angular tour
    
    * Fixed Angular Tour

--- Angular2/src/app/pages/dashboard/home/home.component.ts
+++ Angular2/src/app/pages/dashboard/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import { JoyrideService } from 'ngx-joyride';
 
 import { Card } from './home';
 import { CardItem } from './home.model';
@@ -12,9 +13,20 @@ import { CardItem } from './home.model';
 export class HomeComponent implements OnInit {
 
   cardItems = [];
+  title = 'Sample';
+
+  constructor(private readonly joyride: JoyrideService) { }
 
   ngOnInit(): void {
     console.log(localStorage.getItem('platformId'));
+  }
+
+  tour()
+  {
+    this.joyride.startTour(
+      { steps: ['firstStep', 'secondStep', 'thirdStep', 'forthStep']} // Your steps order
+    );
+    console.log('this is wrking')
   }
 
  initialize(): void {
