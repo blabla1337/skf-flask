c1d072d4c62e2bc68949319a8dbac24c739419a4
Akash M <akashm1999@gmail.com>
2020-08-03 22:12:00 +0530

    Made changes in UI and Added form alert

--- Angular2/src/app/pages/checklists/manage/manage.component.ts
+++ Angular2/src/app/pages/checklists/manage/manage.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { DndDropEvent } from 'ngx-drag-drop';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 import { manageData } from './data';
 import { Manage } from './manage.model';
@@ -20,44 +20,26 @@ export class CheckManageComponent implements OnInit {
   // bread crumb items
   breadCrumbItems: Array<{}>;
 
-  constructor() { }
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit() {
     this.breadCrumbItems = [{ label: 'Checklists' }, { label: 'Manage', active: true }];
 
-
     this._fetchData();
-  }
-
-  /**
-   * on dragging
-   * @param item item dragged
-   * @param list list from item dragged
-   */
-  onDragged(item: any, list: any[]) {
-    const index = list.indexOf(item);
-    list.splice(index, 1);
-  }
-
-  /**
-   * On drop event
-   */
-  onDrop(event: DndDropEvent, filteredList?: any[], targetStatus?: string) {
-    if (filteredList && event.dropEffect === 'move') {
-      let index = event.index;
-
-      if (typeof index === 'undefined') {
-        index = filteredList.length;
-      }
-
-      filteredList.splice(index, 0, event.data);
-    }
   }
 
   private _fetchData() {
     this.checkTotal = manageData.filter(t => t.status === 'Completed');
     this.checkItems = manageData.filter(t => t.status === 'Pending');
     this.checkData = manageData;
+  }
+
+  /**
+   * Open center modal
+   * @param centerDataModal center modal data
+   */
+  centerModal(centerDataModal: any) {
+    this.modalService.open(centerDataModal, { centered: true, size: 'lg' });
   }
 
 }
