bf096cf5e0be54f73e17b3ee8d43ea87d5b7c814
Glenn ten Cate <glenntencate@gmail.com>
2020-08-07 12:07:49 +0200

    Update user.service.ts

--- Angular2/src/app/core/services/user.service.ts
+++ Angular2/src/app/core/services/user.service.ts
@@ -1,12 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 import { Auth } from '../models/auth.model';
+import { environment } from '../../../environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-constructor() { }
+  constructor(
+    private http: HttpClient,
+) { }
+
+public authHeader = new HttpHeaders({ 'Content-Type': 'application/json', 'Authorization': sessionStorage.getItem("Authorization") });
 
 addUser(user: Auth) {
   let users = [];
@@ -17,6 +25,10 @@ addUser(user: Auth) {
     users = [user];
   }
   localStorage.setItem('Users', JSON.stringify(users));
+}
+
+activateUser(value: any, user_id: number): Observable<Object> {
+    return this.http.put(environment.API_ENDPOINT + `/user/activate/${user_id}`, value, { headers: this.authHeader })
 }
 
 }
