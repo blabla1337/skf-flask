0a59d26dfc4d5cadd0e93e8d88aba0ee9e70f068
Akash M <akashm1999@gmail.com>
2020-08-11 18:31:24 +0530

    Added Index Page

---
+++ Angular2/src/app/pages/search/index/index.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-index',
+  templateUrl: './index.component.html',
+  styleUrls: ['./index.component.scss']
+})
+export class IndexComponent implements OnInit {
+
+  // bread crumb items
+  breadCrumbItems: Array<{}>;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.breadCrumbItems = [{ label: 'Search' }, { label: 'Index', active: true }];
+  }
+
+}
