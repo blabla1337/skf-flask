8c82671f2a475782ee02f111735f511696ede554
Akash M <akashm1999@gmail.com>
2020-08-11 20:24:23 +0530

    Added Search Index Page

---
+++ Angular2/src/app/pages/search/search.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SearchRoutingModule } from './search-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SearchRoutingModule
+  ]
+})
+export class SearchModule { }
