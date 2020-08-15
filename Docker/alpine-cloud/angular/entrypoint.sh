81575e159475e07b65d10c52c9ccef9aea8048fd
Glenn ten Cate <glenntencate@gmail.com>
2020-08-15 11:05:26 +0200

    fix typo

--- Docker/alpine-cloud/angular/entrypoint.sh
+++ Docker/alpine-cloud/angular/entrypoint.sh
@@ -2,11 +2,10 @@
 
 set -ex
 
-ORIGIN=${FRONTEND_URI:-http://127.0.0.1:8788}
+ORIGIN=${FRONTEND_URI:-http://127.0.0.1:8888}
 
 echo "angular will try to contact the api at: ${ORIGIN}/api"
-find /usr/share/nginx/html -type f -exec sed -i -e "s,http://127.0.0.1:8888/api,${ORIGIN}/api,g" {} \;
-
+find /usr/share/nginx/html -type f -exec sed -i -e "s,http://127.0.0.1:8888,${ORIGIN},g" {} \;
 cp $1 /etc/nginx/conf.d/site.conf
 cat /etc/nginx/conf.d/site.conf
 
