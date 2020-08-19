23fa42e0a55cb6cbbd16ca7cb697213b5af002a4
Akash M <akashm1999@gmail.com>
2020-08-19 19:39:18 +0530

    Fixed an error

--- Angular2/src/app/pages/checklists/manage/manage.component.ts
+++ Angular2/src/app/pages/checklists/manage/manage.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
-import { ChecklistCategoryService } from '../../../core/services/checklist_category.service'
+import { manageData } from './data';
+import { Manage } from './manage.model';
 
 @Component({
   selector: 'app-manage',
   templateUrl: './manage.component.html',
   styleUrls: ['./manage.component.scss']
 })
-export class CheckManageComponent implements OnInit
-{
+export class CheckManageComponent implements OnInit {
 
+  checkTotal: Manage[];
+  checkItems: Manage[];
   checkData;
 
   public isCollapsed: boolean[] = [];
@@ -18,25 +20,25 @@ export class CheckManageComponent implements OnInit
   // bread crumb items
   breadCrumbItems: Array<{}>;
 
-  constructor(
-    private modalService: NgbModal,
-    private _checklistCategoryService: ChecklistCategoryService,
-  ) { }
+  constructor(private modalService: NgbModal) { }
 
-  ngOnInit()
-  {
+  ngOnInit() {
     this.breadCrumbItems = [{ label: 'Checklists' }, { label: 'Manage', active: true }];
 
+    this._fetchData();
   }
 
-  getChecklistCategories()
-  {
-    this._checklistCategoryService
-    .getChecklistCategoryCollection(Number(localStorage.getItem("categorySelector"))).subscribe()
+  private _fetchData() {
+    this.checkTotal = manageData.filter(t => t.status === 'Completed');
+    this.checkItems = manageData.filter(t => t.status === 'Pending');
+    this.checkData = manageData;
   }
 
-  centerModal(centerDataModal: any)
-  {
+  /**
+   * Open center modal
+   * @param centerDataModal center modal data
+   */
+  centerModal(centerDataModal: any) {
     this.modalService.open(centerDataModal, { centered: true, size: 'lg' });
   }
 
