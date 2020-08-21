e152319c8222d3092d031dc9ee6c51e7be1e45b8
Akash M <akashm1999@gmail.com>
2020-08-21 18:33:09 +0530

    Modified Checklists View UI

--- Angular2/src/app/pages/checklists/view/view.component.ts
+++ Angular2/src/app/pages/checklists/view/view.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgxSpinnerService  } from 'ngx-spinner';
 
 import { ChecklistService } from '../../../core/services/checklists.service';
+import { ChecklistCategoryService } from '../../../core/services/checklist_category.service';
 
 @Component({
   selector: 'app-view',
@@ -16,15 +18,22 @@ export class ViewComponent implements OnInit
   public queryString;
   public checklistData: any;
   public delete: string;
+  public catSelector: number;
+  public categoryData: any = [];
 
   constructor(
     private modalService: NgbModal,
-    private _checklistService: ChecklistService
+    // tslint:disable-next-line: variable-name
+    private _checklistService: ChecklistService,
+    // tslint:disable-next-line: variable-name
+    private _checklistCategoryService: ChecklistCategoryService,
+    private spinner: NgxSpinnerService,
   ) { }
 
   ngOnInit()
   {
     this.breadCrumbItems = [{ label: 'Checklists' }, { label: 'View', active: true }];
+    this.catSelector = Number(localStorage.getItem('categorySelector'));
     this._fetchData();
   }
 
@@ -33,21 +42,36 @@ export class ViewComponent implements OnInit
    */
   private _fetchData()
   {
+    this.spinner.show();
     this._checklistService
       .getChecklistsCollection(Number(localStorage.getItem('categorySelector')))
-      .subscribe(checklist => this.checklistData = checklist);
+      .subscribe(checklist => {
+        this.checklistData = checklist;
+        this.spinner.hide();
+      });
+
+    this._checklistCategoryService
+      .getChecklistCategoryCollection()
+      .subscribe(data => this.categoryData = data);
+  }
+
+  // tslint:disable-next-line: ban-types
+  setCategorySelectorId(categoryId: Number)
+  {
+    localStorage.setItem('categorySelector', categoryId.toString());
+    this._fetchData();
   }
 
   deleteChecklistType(id: number)
   {
-    if (this.delete == 'DELETE') {
-      this._checklistService.deleteChecklistType(id).subscribe(x => this._fetchData())
+    if (this.delete === 'DELETE') {
+      this._checklistService.deleteChecklistType(id).subscribe(x => this._fetchData());
     }
   }
 
   showModal(content: any)
   {
-    this.modalService.open(content, { size: 'lg', centered: true });
+    this.modalService.open(content, { size: 'sm', centered: true });
   }
 
 }
