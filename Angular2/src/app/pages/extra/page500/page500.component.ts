4bb6cd97d76e847d4dbbb88bf073c32d3d126fdf
Akash M <akashm1999@gmail.com>
2020-08-23 23:18:41 +0530

    Added 404 Page

---
+++ Angular2/src/app/pages/extra/page500/page500.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page500',
+  templateUrl: './page500.component.html',
+  styleUrls: ['./page500.component.scss']
+})
+export class Page500Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
