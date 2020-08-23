ddb236fb5ae23e507c1ab75040de8a6614ee9c1e
Akash M <akashm1999@gmail.com>
2020-08-23 23:18:41 +0530

    Added 404 Page

--- Angular2/src/app/app-routing.module.ts
+++ Angular2/src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LayoutComponent } from './layouts/layout.component';
+import { Page404Component } from './pages/extra/page404/page404.component';
 
 const routes: Routes = [
   { path: '', component: LayoutComponent, loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule)},
   { path: 'auth', loadChildren: () => import('./pages/auth/auth.module').then(m => m.AuthModule) },
+  { path: '**', component: Page404Component},
+
 
 ];
 
--- Angular2/src/app/pages/pages-routing.module.ts
+++ Angular2/src/app/pages/pages-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './dashboard/home/home.component';
+
 const routes: Routes = [
   { path: '', redirectTo: 'dashboard' },
   { path: 'dashboard', component: HomeComponent },
