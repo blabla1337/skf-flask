531ed7b223f5ccd56997d829d09c695404513952
Akash M <akashm1999@gmail.com>
2020-09-05 22:07:34 +0530

    Added Auth Test cases

--- Angular2/src/app/pages/auth/register/register.component.spec.ts
+++ Angular2/src/app/pages/auth/register/register.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { ReactiveFormsModule, FormsModule, FormBuilder } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule, Validators, FormBuilder } from '@angular/forms';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
@@ -31,4 +31,66 @@ describe('RegisterComponent', () =>
   {
     expect(component).toBeTruthy();
   });
+
+  it('form invalid when empty', () => {
+    expect(component.registerationForm.valid).toBeFalse();
+  });
+
+  it('username field validity', () => {
+    let username = component.registerationForm.controls['username'];
+    expect(username.valid).toBeFalsy();
+
+    let errors = {};
+    errors = username.errors || {};
+    expect(errors['required']).toBeTruthy();
+  });
+
+  it('email field validity', () => {
+    let email = component.registerationForm.controls['email'];
+    expect(email.valid).toBeFalsy();
+
+    let errors = {};
+    errors = email.errors || {};
+    expect(errors['required']).toBeTruthy();
+  });
+
+  it('password field validity', () => {
+    let password = component.registerationForm.controls['password'];
+    expect(password.valid).toBeFalsy();
+
+    let errors = {};
+    errors = password.errors || {};
+    expect(errors['required']).toBeTruthy();
+
+    password.setValue('1234567');
+    errors = password.errors || {};
+    expect(errors['minlength']).toBeDefined();
+  });
+
+  it('userid field validity', () => {
+    let user_id = component.registerationForm.controls['user_id'];
+    expect(user_id.valid).toBeFalsy();
+
+    let errors = {};
+    errors = user_id.errors || {};
+    expect(errors['required']).toBeTruthy();
+
+    user_id.setValue('1a-/');
+    errors = user_id.errors || {};
+    expect(errors['pattern']).toBeTruthy();
+  });
+
+  it('access token field validity', () => {
+    let accessToken = component.registerationForm.controls['accessToken'];
+    expect(accessToken.valid).toBeFalsy();
+
+    let errors = {};
+    errors = accessToken.errors || {};
+    expect(errors['required']).toBeTruthy();
+
+    accessToken.setValue('1a-/');
+    errors = accessToken.errors || {};
+    expect(errors['pattern']).toBeTruthy();
+  });
+
 });
