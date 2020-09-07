76fb76ed6c800443563cc7aa782f047af21b606b
Akash M <akashm1999@gmail.com>
2020-09-07 10:53:44 +0530

    Written Unit Test for Code Example Update

--- Angular2/src/app/pages/code-example/update/update.component.spec.ts
+++ Angular2/src/app/pages/code-example/update/update.component.spec.ts
@@ -1,22 +1,33 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, tick, fakeAsync } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 import { UpdateCodeComponent } from './update.component';
+import { ViewCodeComponent } from '../view/view.component';
 
 describe('UpdateCodeComponent', () =>
 {
   let component: UpdateCodeComponent;
   let fixture: ComponentFixture<UpdateCodeComponent>;
+  let router: Router;
+  let location: Location;
 
   beforeEach(async(() =>
   {
     TestBed.configureTestingModule({
-      imports: [ReactiveFormsModule, FormsModule, HttpClientTestingModule, RouterTestingModule],
+      imports: [ReactiveFormsModule, FormsModule, HttpClientTestingModule,
+                RouterTestingModule.withRoutes([
+                  { path: 'code-example/view', component: ViewCodeComponent },
+                ])],
       declarations: [UpdateCodeComponent]
     })
       .compileComponents();
+    router = TestBed.inject(Router);
+    location = TestBed.inject(Location);
+    router.initialNavigation();
   }));
 
   beforeEach(() =>
@@ -31,4 +42,15 @@ describe('UpdateCodeComponent', () =>
   {
     expect(component).toBeTruthy();
   });
+
+  it('should update CodeExample Item', fakeAsync(() =>
+  {
+    component.updateCodeExampleItem();
+    expect(component.isSubmitted ).toBeTruthy();
+    expect(component.codeExampleForm.valid).toBeFalsy();
+
+    router.navigate(['code-example/view']);
+    tick();
+    expect(location.path()).toBe('/code-example/view');
+  }));
 });
