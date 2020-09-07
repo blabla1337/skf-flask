ead67010577c4fa16be666fcd367a57a0f958e70
Akash M <akashm1999@gmail.com>
2020-09-07 14:33:40 +0530

    Written Unit Test for Checklists View and Update

--- Angular2/src/app/pages/users/create/create.component.spec.ts
+++ Angular2/src/app/pages/users/create/create.component.spec.ts
@@ -1,22 +1,33 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, tick, fakeAsync } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 import { UserCreateComponent } from './create.component';
+import { ManageComponent } from '../manage/manage.component';
 
 describe('UserCreateComponent', () =>
 {
     let component: UserCreateComponent;
     let fixture: ComponentFixture<UserCreateComponent>;
+    let router: Router;
+    let location: Location;
 
     beforeEach(async(() =>
     {
         TestBed.configureTestingModule({
-            imports: [ReactiveFormsModule, FormsModule, HttpClientTestingModule, RouterTestingModule],
+            imports: [ReactiveFormsModule, FormsModule, HttpClientTestingModule,
+                    RouterTestingModule.withRoutes([
+                        { path: 'users/manage', component: ManageComponent },
+                    ])],
             declarations: [UserCreateComponent]
         })
             .compileComponents();
+        router = TestBed.inject(Router);
+        location = TestBed.inject(Location);
+        router.initialNavigation();
     }));
 
     beforeEach(() =>
@@ -38,10 +49,14 @@ describe('UserCreateComponent', () =>
         expect(component.submit).toBeTruthy();
     });
 
-    it('form invalid when empty', () => {
+    it('form invalid when empty', fakeAsync(() => {
         component.newUser();
         expect(component.userForm.invalid).toBeTruthy();
-    });
+
+        router.navigate(['users/manage']);
+        tick();
+        expect(location.path()).toBe('/users/manage');
+    }));
 
     it('priviledge field validity', () => {
         let priviledge = component.userForm.controls['privilege_id'];
