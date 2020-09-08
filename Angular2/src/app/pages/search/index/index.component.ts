2b13661f81a17d8cb9a374b2ba07d61d85ce9cf2
Glenn ten Cate <glenntencate@gmail.com>
2020-09-08 15:20:34 +0200

    testing search

--- Angular2/src/app/pages/search/index/index.component.ts
+++ Angular2/src/app/pages/search/index/index.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, DoCheck, AfterViewInit, AfterViewChecked } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner';
 
 import { SearchService } from '../../../core/services/search.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.scss']
 })
-export class IndexComponent implements OnInit {
+
+export class IndexComponent implements OnInit, AfterViewChecked {
 
   // bread crumb items
   public breadCrumbItems: Array<{}>;
@@ -17,16 +19,28 @@ export class IndexComponent implements OnInit {
   public results_labs: any = [];
   public results_kb: any = [];
   public results_projects: any = [];
+  public search: string;
+  public check: boolean;
 
   constructor(
     private router: Router,
     private _searchService: SearchService,
     private spinner: NgxSpinnerService, 
-  ) { }
+  ) {  }
 
   ngOnInit(): void {
     this.breadCrumbItems = [{ label: 'Search' }, { label: 'Index', active: true }];
     this._fetchData();
+    this.router.ngOnDestroy();
+    this.search = localStorage.getItem('search');
+  }
+
+  
+  ngAfterViewChecked(): void {
+    if (localStorage.getItem('search') != this.search) {
+     // not working its calling it 1000x times... 
+     //this._fetchData();
+    }
   }
 
   /**
