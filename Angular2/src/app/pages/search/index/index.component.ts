154a908ee38beeae2eb22cae28e1416cf83fc493
Riiecco <r.tencate77@gmail.com>
2020-09-21 14:40:23 +0200

    dirty search fix

--- Angular2/src/app/pages/search/index/index.component.ts
+++ Angular2/src/app/pages/search/index/index.component.ts
@@ -11,7 +11,8 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./index.component.scss']
 })
 
-export class IndexComponent implements OnInit, AfterViewChecked {
+export class IndexComponent implements OnInit, AfterViewChecked
+{
 
   // bread crumb items
   public breadCrumbItems: Array<{}>;
@@ -30,62 +31,73 @@ export class IndexComponent implements OnInit, AfterViewChecked {
   constructor(
     private router: Router,
     private _searchService: SearchService,
-    private spinner: NgxSpinnerService, 
-  ) {  }
+    private spinner: NgxSpinnerService,
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void
+  {
     this.breadCrumbItems = [{ label: 'Search' }, { label: 'Index', active: true }];
-    //this.getCheck();
+    this.getCheck();
     this.search = localStorage.getItem('search');
     this.length = this.results_checklist.length;
     console.log(this.length);
   }
 
 
-  ngAfterViewChecked(): void {
+  ngAfterViewChecked(): void
+  {
     if (localStorage.getItem('search') != this.search) {
-     // not working its calling it 1000x times... 
-     //this._fetchData();
+
+
+      this.search = localStorage.getItem('search');
+
+      window.location.reload();
     }
   }
 
 
-  getCheck(){
+  getCheck()
+  {
     this.check_status = true;
     this.lab_status = false;
     this.kb_status = false;
     this.pro_status = false;
-    if(localStorage.getItem("search") !== ""){
+    if (localStorage.getItem("search") !== "") {
       this.spinner.show();
-      this._searchService.searchChecklist(localStorage.getItem("search")).subscribe(results => {
+      this._searchService.searchChecklist(localStorage.getItem("search")).subscribe(results =>
+      {
         this.results_checklist = results;
         this.spinner.hide();
       });
     }
   }
 
-  getKB(){
+  getKB()
+  {
     this.lab_status = false;
     this.kb_status = true;
     this.pro_status = false;
     this.check_status = false;
-    if(localStorage.getItem("search") !== ""){
+    if (localStorage.getItem("search") !== "") {
       this.spinner.show();
-      this._searchService.searchKb(localStorage.getItem("search")).subscribe(results => {
+      this._searchService.searchKb(localStorage.getItem("search")).subscribe(results =>
+      {
         this.results_kb = results;
         this.spinner.hide();
       });
     }
   }
 
-  getLab(){
+  getLab()
+  {
     this.kb_status = false;
     this.pro_status = false;
     this.check_status = false;
     this.lab_status = true;
-    if(localStorage.getItem("search") !== ""){
+    if (localStorage.getItem("search") !== "") {
       this.spinner.show();
-      this._searchService.searchLabs(localStorage.getItem("search")).subscribe(results => {
+      this._searchService.searchLabs(localStorage.getItem("search")).subscribe(results =>
+      {
         this.results_labs = results;
         console.log(this.results_labs)
         this.spinner.hide();
@@ -93,14 +105,16 @@ export class IndexComponent implements OnInit, AfterViewChecked {
     }
   }
 
-  getProj(){
+  getProj()
+  {
     this.pro_status = true;
     this.check_status = false;
     this.lab_status = false;
     this.kb_status = false;
-    if(localStorage.getItem("search") !== ""){
+    if (localStorage.getItem("search") !== "") {
       this.spinner.show();
-      this._searchService.searchProject(localStorage.getItem("search")).subscribe(results => {
+      this._searchService.searchProject(localStorage.getItem("search")).subscribe(results =>
+      {
         this.results_projects = results;
         this.spinner.hide();
       });
