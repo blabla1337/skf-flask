2faf784d127e3a206ac067bacc53d4fe1034a1bc
Glenn ten Cate <glenntencate@gmail.com>
2020-10-22 15:52:13 +0200

    Fix skip login provider

--- Docker/alpine-cloud/angular/entrypoint.sh
+++ Docker/alpine-cloud/angular/entrypoint.sh
@@ -3,10 +3,11 @@
 set -ex
 
 ORIGIN=${FRONTEND_URI:-http://127.0.0.1:8888}
+SKIP=${SKIP_LOGIN:-skfprovider}
 
 echo "angular will try to contact the api at: ${ORIGIN}/api"
 find /usr/share/nginx/html -type f -exec sed -i -e "s,http://127.0.0.1:8888,${ORIGIN},g" {} \;
 cp $1 /etc/nginx/conf.d/site.conf
 cat /etc/nginx/conf.d/site.conf
-find /usr/share/nginx/html -type f -exec sed -i -e "s,skiploginprovider,skfprovider,g" {} \;
+find /usr/share/nginx/html -type f -exec sed -i -e "s,skiploginprovider,${SKIP},g" {} \;
 exec nginx -g "daemon off;"
