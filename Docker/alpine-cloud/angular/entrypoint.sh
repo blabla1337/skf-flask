cafdf5296aa8eeb00be413597c23f8dba893a18e
Glenn ten Cate <glenntencate@gmail.com>
2020-10-22 17:11:52 +0200

    fix

--- Docker/alpine-cloud/angular/entrypoint.sh
+++ Docker/alpine-cloud/angular/entrypoint.sh
@@ -9,5 +9,5 @@ echo "angular will try to contact the api at: ${ORIGIN}/api"
 find /usr/share/nginx/html -type f -exec sed -i -e "s,http://127.0.0.1:8888,${ORIGIN},g" {} \;
 cp $1 /etc/nginx/conf.d/site.conf
 cat /etc/nginx/conf.d/site.conf
-find /usr/share/nginx/html -type f -exec sed -i -e "s,skiploginprovider,${SKIP},g" {} \;
+find /usr/share/nginx/html -type f -exec sed -i -e "s,skfprovider,${SKIP},g" {} \;
 exec nginx -g "daemon off;"
