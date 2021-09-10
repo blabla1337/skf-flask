bc58674b0ca0dedf0b1f8bc7589d835ce4d2bfc0
Jacob O'Toole <jacob.otoole1@gmail.com>
2021-09-10 16:30:47 +0100

    Move shared functions into `common.py` and delete ingress

--- skf/rabbit_mq_workers/deletion-worker.py
+++ skf/rabbit_mq_workers/deletion-worker.py
@@ -1,55 +1,23 @@
 #!/usr/bin/env python
-import pika, time, random, yaml
-from os import path
+import pika
 from skf import settings
-from kubernetes import client, config
+
+from common import delete_all
 
 creds = pika.PlainCredentials('admin', 'admin-skf-secret')
-connection = pika.BlockingConnection(pika.ConnectionParameters(host=settings.RABBIT_MQ_CONN_STRING, credentials=creds))
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+    host=settings.RABBIT_MQ_CONN_STRING,
+    credentials=creds,
+))
 channel = connection.channel()
 channel.queue_declare(queue='deletion_qeue')
-
 
 def delete_container(rpc_body):
     user_id = string_split_user_id(rpc_body)
     deployment = string_split_deployment(rpc_body)
-    delete_deployment(deployment, user_id)
-    delete_service(deployment, user_id)
+    delete_all(deployment, user_id)
     time.sleep(3)
     return {'message': 'If present, the container image was deleted from the cluster!'} 
-
-
-def delete_deployment(instance_name, user_id):
-    try:
-        config.load_kube_config()
-        api_instance = client.AppsV1Api()
-        api_response = api_instance.delete_namespaced_deployment(
-            name=instance_name,
-            namespace=user_id,
-            body=client.V1DeleteOptions(
-                propagation_policy='Foreground',
-                grace_period_seconds=5))
-        print("Deployment deleted. status='%s'" % str(api_response.status))
-        return {'message': 'Deployment deleted.'} 
-    except:
-        return {'message': 'Kubernetes configuration is either missing or done incorrectly, error deployment delete!'} 
-
-
-def delete_service(instance_name, user_id):
-    try:
-        config.load_kube_config()
-        api_instance = client.CoreV1Api()
-        api_response = api_instance.delete_namespaced_service(
-            name=instance_name,
-            namespace=user_id,
-            body=client.V1DeleteOptions(
-                propagation_policy='Foreground',
-                grace_period_seconds=5))
-        print("Deployment deleted. status='%s'" % str(api_response.status))
-        return {'message': 'Deployment deleted.'}         
-    except:
-        return {'message': 'Kubernetes configuration is either missing or done incorrectly, error service delete!'} 
-
 
 def string_split_user_id(body):
     try:
