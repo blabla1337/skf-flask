181c68165717ee870cebf30a10115e19ef3571c4
Ionut Breaz <ibreaz@yahoo.com>
2022-02-26 09:34:30 +0200

    ids are guids in the sample and in model get and set seenItems

--- Angular2/src/app/core/services/training.service.ts
+++ Angular2/src/app/core/services/training.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Course} from '../models/course.model';
 import {Observable, of} from 'rxjs';
+import _ from "lodash";
 
 @Injectable()
 export class TrainingService {
@@ -9,16 +10,30 @@ export class TrainingService {
     private http: HttpClient,
   ) { }
 
-  getCourse(id: number): Observable<Course>
-  {
+  // TODO IB this should be by courseId
+  seenItems: string[] = [];
+
+  // TODO IB !!!! test this
+  setCourseSeenItems(courseId: string, itemIds: string[]): Observable<boolean> {
+    this.seenItems.push(...itemIds)
+    this.seenItems = _.uniq(this.seenItems)
+    return of(true)
+  }
+
+  // TODO IB !!!! test this
+  getCourseSeenItems(courseId: string): Observable<string[]> {
+    return of(this.seenItems)
+  }
+
+  getCourse(courseId: string): Observable<Course> {
     return of(TrainingService.getSampleCourse());
   }
 
   private static getSampleCourse(): Course {
     return {
-      "id": 1,
-      "version": 2,
-      "date": "18/03/2022",
+      "id": "84c62728-c371-4d51-adb5-21a53d366474",
+      "version": 2.1,
+      "date": "25/02/2022",
       "name": "Secure Development",
       "languages": [
         "python",
@@ -33,7 +48,7 @@ export class TrainingService {
       ],
       "topics": [
         {
-          "id": 2,
+          "id": "c2aeef96-4d64-4360-a16a-95e32c1c5a5c",
           "name": "Session management",
           "content": [
             {
@@ -42,7 +57,7 @@ export class TrainingService {
           ],
           "categories": [
             {
-              "id": 3,
+              "id": "bd87bbcd-2fc5-4619-a782-9aa6400a3d4d",
               "name": "Session Hijacking",
               "content": [
                 {
@@ -74,7 +89,7 @@ export class TrainingService {
               ]
             },
             {
-              "id": 4,
+              "id": "ac1eb377-7e5f-4eae-9757-45f9c8b3b058",
               "name": "HTTP Only flag",
               "content": [
                 {
@@ -86,7 +101,7 @@ export class TrainingService {
               ]
             },
             {
-              "id": 5,
+              "id": "6d102708-f900-4665-98c6-9efb3ac56fb4",
               "name": "Session riding",
               "content": [
                 {
@@ -100,7 +115,7 @@ export class TrainingService {
           ]
         },
         {
-          "id": 6,
+          "id": "0f62ea67-476f-46d2-956d-110546492ab3",
           "name": "Validation, Sanitization and Encoding",
           "content": [
             {
@@ -109,7 +124,7 @@ export class TrainingService {
           ],
           "categories": [
             {
-              "id": 7,
+              "id": "cd08670b-64fe-412a-92cb-6e5dac00959e",
               "name": "SQL injection",
               "content": [
                 {
@@ -121,7 +136,7 @@ export class TrainingService {
               ]
             },
             {
-              "id": 8,
+              "id": "905b38c5-54a3-4b82-985e-b9812a2c9ce4",
               "name": "LDAP injection",
               "content": [
                 {
@@ -133,7 +148,7 @@ export class TrainingService {
               ]
             },
             {
-              "id": 9,
+              "id": "218b1d8e-803b-4fc1-b05a-e7ce4a0daae2",
               "name": "Server Side Template injection",
               "content": [
                 {
@@ -145,7 +160,7 @@ export class TrainingService {
               ]
             },
             {
-              "id": 10,
+              "id": "c04099d4-ee8d-4c35-adc7-8f4a5a98a49e",
               "name": "Server Side Template injection",
               "content": [
                 {
