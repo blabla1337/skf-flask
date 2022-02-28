837716f686dbb9eb7dd7ef723eacc124a49138b1
Ionut Breaz <ibreaz@yahoo.com>
2022-02-28 20:29:39 +0200

    profiles have guids

--- Angular2/src/app/core/services/training.service.ts
+++ Angular2/src/app/core/services/training.service.ts
@@ -6,9 +6,7 @@ import _ from "lodash";
 
 @Injectable({providedIn: 'root'})
 export class TrainingService {
-  constructor(
-    private http: HttpClient,
-  ) { }
+  constructor(private http: HttpClient) { }
 
   // TODO IB this should be by courseId
   private seenItems: string[] = [];
@@ -184,21 +182,21 @@ export class TrainingService {
   private static getProfiles(): Profile[] {
     return [
       {
-        id: "id1",
+        id: "4a03cbe7-2aa9-4b16-b4d6-55d125a550f2",
         text: `This profile is dedicated for developers who want to learn secure development.
         This course is based on the ASVS and the secure coding principles to teach the best practices.`,
         iconClass: "bx-code-alt",
         startButtonText: "Start secure development course"
       },
       {
-        id: "id2",
+        id: "12902ddc-e189-4d8c-a7fb-dbc70c3e43dd",
         text: `This profile is dedicated for security pentesters who want to learn the the basics and advanced techniques
         of penetration testing for Web/API applications. This course is based on the OWASP WSTG and teach you how to perform a good quality penetrationt test.`,
         iconClass: "bx-shield",
         startButtonText: "Start hacking course"
       },
       {
-        id: "id3",
+        id: "67fbf0a6-2b00-4983-a48a-4b0c7f0cdb8c",
         text: `This profile is dedicated for Ops and Infra people who want to learn about the server hardening and security best practices.
         This course is based on the VulnHub materials and lessons to teach the best practices for server configurations and hardening.`,
         iconClass: "bx-slider-alt",
