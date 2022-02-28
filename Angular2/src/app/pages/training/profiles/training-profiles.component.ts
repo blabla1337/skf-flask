921c1a69e299d47d0c980f4231ae437261cfd2ee
Ionut Breaz <ibreaz@yahoo.com>
2022-02-28 18:32:39 +0200

    profiles are dynamic

--- Angular2/src/app/pages/training/profiles/training-profiles.component.ts
+++ Angular2/src/app/pages/training/profiles/training-profiles.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Profile} from '../../../core/models/course.model';
+import {TrainingService} from '../../../core/services/training.service';
 
 @Component({
   selector: 'app-training-profiles',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./training-profiles.component.scss']
 })
 export class TrainingProfilesComponent implements OnInit {
+  profiles: Profile[] = []
 
-  constructor() { }
+  constructor(private trainingService: TrainingService) { }
 
   ngOnInit(): void {
+    this.trainingService.getProfilesList().subscribe(profiles => {
+      this.profiles = profiles;
+    } )
+
   }
 
 }
