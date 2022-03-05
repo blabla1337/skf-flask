a0197b759949bc619215745b57e8d32612ad4ce5
Ionut Breaz <ibreaz@yahoo.com>
2022-03-05 12:18:00 +0200

    implement selection in the tree show name of the current CourseItem

--- Angular2/src/app/pages/training/course-tree/training-course-tree.component.ts
+++ Angular2/src/app/pages/training/course-tree/training-course-tree.component.ts
@@ -1,44 +1,57 @@
-import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {Subscription} from 'rxjs';
 import {TrainingService} from '../../../core/services/training.service';
-import {Course} from '../../../core/models/course.model';
-import {TreeComponent} from '@circlon/angular-tree-component';
+import {Course, CourseItem} from '../../../core/models/course.model';
+import {TreeComponent, TreeNode} from '@circlon/angular-tree-component';
+import {TrainingPersistenceService} from '../../../core/services/training.persistence.service';
 
 @Component({
   selector: 'app-training-course-tree',
   templateUrl: './training-course-tree.component.html',
   styleUrls: ['./training-course-tree.component.scss']
 })
-export class TrainingCourseTreeComponent implements OnInit, OnDestroy {
+export class TrainingCourseTreeComponent implements OnInit, AfterViewInit, OnDestroy {
   @ViewChild(TreeComponent) private tree: TreeComponent;
-
   private subscriptions: Subscription[] = [];
-  private course: Course;
   public nodes = [];
   public options = {};
 
-  constructor(private trainingService: TrainingService) { }
+  constructor(private trainingService: TrainingService,
+              private trainingPersistenceService: TrainingPersistenceService) {
+
+  }
 
   ngOnInit(): void {
     this.subscriptions.push(this.trainingService.getCourse("").subscribe(course => {
-      this.course = course;
-      this.nodes = course.topics.map(t => ({
-        id: t.id,
-        name: t.name,
-        isExpanded: true,
-        content: t.content,
-        children: t.categories.map(c => ({
-          id: c.id,
-          name: c.name,
-          content: c.content,
-          children:[]
-        }))
+      this.setNodesFromCourse(course);
+    }));
+  }
+
+  private setNodesFromCourse(course: Course) {
+    this.nodes = course.topics.map(t => ({
+      id: t.id,
+      name: t.name,
+      isExpanded: true,
+      content: t.content,
+      children: t.categories.map(c => ({
+        id: c.id,
+        name: c.name,
+        content: c.content,
+        children:[]
       }))
     }));
+  }
 
-    setTimeout(() => {
-      this.tree.treeModel.focusNextNode();
-    }, 0)
+  ngAfterViewInit() {
+    if (this.nodes.length > 0) {
+      this.tree.treeModel.roots[0].setActiveAndVisible();
+    }
+  }
+
+  onActivate(event: any) {
+    const treeNode: TreeNode = event.node;
+    const courseItem: CourseItem = treeNode.data;
+    this.trainingPersistenceService.setSelectedCourseItem(courseItem);
   }
 
   ngOnDestroy(): void {
