b57924012e598699d4b1c8ae76587d9aa2e95389
Alexandru Stanciu <alexandru.stanciu@gmail.com>
2022-03-06 20:45:47 +0200

    training items endpoint integration

--- skf/api/training/business.py
+++ skf/api/training/business.py
@@ -1,7 +1,4 @@
-from skf.database import db
 from skf.api.security import log, val_num, val_float, val_alpha_num, val_alpha_num_special
-# from skf.database.training import Profile, Course, Topic, Category
-# from sqlalchemy import desc, asc
 from flask import current_app as app
 from functools import cache
 import sys, json, yaml
@@ -9,7 +6,17 @@ import sys, json, yaml
 
 @cache
 def get_training_profiles():
-    log("User requested list training profiles", "LOW", "PASS")
+    result = {}
     with app.open_resource('training/profiles.yml') as f:
-        result = json.dumps(yaml.safe_load(f.read()))
+        p_yaml = yaml.safe_load(f.read())
+        p_json = json.dumps(p_yaml)
+        result = json.loads(p_json)
+    return result
+
+def get_training_profile_items():
+    log("User requested list training profiles", "LOW", "PASS")
+    result = get_training_profiles()
+    for profile in result['profiles']:
+        if 'courses' in profile:
+            del profile['courses']
     return result
