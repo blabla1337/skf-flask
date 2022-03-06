0c4b9eeb3063e4c188e6996a41a182df286289d2
Alexandru Stanciu <alexandru.stanciu@gmail.com>
2022-03-06 20:45:47 +0200

    training items endpoint integration

--- skf/api/training/endpoints/training_profile_items.py
+++ skf/api/training/endpoints/training_profile_items.py
@@ -1,5 +1,5 @@
 from flask_restplus import Resource
-from skf.api.training.business import get_training_profiles
+from skf.api.training.business import get_training_profile_items
 from skf.api.restplus import api
 from skf.api.security import *
 
@@ -10,5 +10,5 @@ class TrainingProfileCollection(Resource):
 
     @api.response(400, 'No results found')
     def get(self):
-        result = get_training_profiles()
+        result = get_training_profile_items()
         return result, 200, security_headers()
