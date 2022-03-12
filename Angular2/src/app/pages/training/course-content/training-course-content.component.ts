75ed775fe800b53ec36f48f44bc7a71014539551
Ionut Breaz <ibreaz@yahoo.com>
2022-03-12 10:29:47 +0200

    load also a youtube video

--- Angular2/src/app/pages/training/course-content/training-course-content.component.ts
+++ Angular2/src/app/pages/training/course-content/training-course-content.component.ts
@@ -13,17 +13,22 @@ export class TrainingCourseContentComponent implements OnInit {
   private subscriptions: Subscription[] = [];
   public courseItem: CourseItem;
   public markdownPath: string;
+  public videoPath: string;
 
   constructor(private trainingPersistenceService: TrainingPersistenceService) { }
 
   ngOnInit(): void {
     this.subscriptions.push(this.trainingPersistenceService.currentCourseItem$.subscribe(courseItem => {
       this.courseItem = courseItem;
+      this.markdownPath = undefined;
+      this.videoPath = undefined;
       if (courseItem && courseItem.content && courseItem.content.length > 0) {
         if (courseItem.content[0].slide) {
           this.markdownPath = this.course.assetsPath + courseItem.content[0].slide;
         } else if (courseItem.content[0].questionnaire) {
           this.markdownPath = this.course.assetsPath + courseItem.content[0].questionnaire;
+        } else if (courseItem.content[0].video) {
+          this.videoPath = courseItem.content[0].video;
         }
       }
     }));
