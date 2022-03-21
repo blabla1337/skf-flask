29ea6cf50063e12ac59a4c83a452aeda18e80028
Ionut Breaz <ibreaz@yahoo.com>
2022-03-21 10:44:27 +0200

    go to next slide

--- Angular2/src/app/pages/training/content-markdown/training-content-markdown.component.ts
+++ Angular2/src/app/pages/training/content-markdown/training-content-markdown.component.ts
@@ -4,6 +4,8 @@ import {NgxSpinnerService} from 'ngx-spinner';
 import {Subscription} from 'rxjs';
 import {TrainingNavigationService} from '../../../core/services/training-navigation.service';
 
+const MARKDOWN_SPLIT_MARKER = "-----SPLIT-----";
+
 @Component({
   selector: 'app-training-content-markdown',
   templateUrl: './training-content-markdown.component.html',
@@ -21,7 +23,9 @@ export class TrainingContentMarkdownComponent implements OnInit, OnDestroy {
     this.updateMarkdown();
   }
 
-  public data: string;
+  public currentDataSlideIndex: number;
+  public dataSlides: string[] = [];
+  public currentDataSlide: string;
 
   constructor(private httpClient: HttpClient,
               private spinner: NgxSpinnerService,
@@ -31,8 +35,12 @@ export class TrainingContentMarkdownComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.subscriptions.push(this.trainingNavigationService.nextClicked$.subscribe(() => {
       console.log('TODO IB !!!! nextClicked$ in markdown');
-      // TODO IB !!!! 1 raise only after all slides are displayed
-      this.trainingNavigationService.raiseNextContentItem();
+      if (this.dataSlides && this.currentDataSlideIndex < this.dataSlides.length - 1) {
+        this.currentDataSlideIndex++;
+        this.prepareCurrentDataSlide();
+      } else {
+        this.trainingNavigationService.raiseNextContentItem();
+      }
     }));
   }
 
@@ -41,17 +49,27 @@ export class TrainingContentMarkdownComponent implements OnInit, OnDestroy {
 
   private updateMarkdown() {
     const headers = new HttpHeaders({'Content-Type': 'text/plain; charset=utf-8'});
-    this.data = undefined;
+    this.dataSlides = [];
+    this.currentDataSlideIndex = 0;
+    this.currentDataSlide = undefined;
     this.spinner.show();
 
     this.httpClient.get(this._markdownPath, {headers, responseType: 'text'})
       .subscribe(data => {
+        console.log('TODO IB !!!! data', data);
         this.spinner.hide();
-        this.data = data;
+        this.dataSlides = data.split(MARKDOWN_SPLIT_MARKER);
+        this.prepareCurrentDataSlide();
       }, (error) => {
         this.spinner.hide();
         console.error(`Could not load markdown data at path ${this._markdownPath}. Error: `, error);
       });
+  }
+
+  private prepareCurrentDataSlide() {
+    if (this.dataSlides && this.currentDataSlideIndex < this.dataSlides.length) {
+      this.currentDataSlide = this.dataSlides[this.currentDataSlideIndex];
+    }
   }
 
   ngOnDestroy(): void {
