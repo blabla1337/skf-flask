9e7ad3d6c5ef1b1a82f81cf8b77e192bd91b615d
Ionut Breaz <ibreaz@yahoo.com>
2022-03-21 08:57:27 +0200

    add lab content and implement Navigation Service

---
+++ Angular2/src/app/pages/training/content-lab/training-content-lab.component.ts
@@ -0,0 +1,30 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Subscription} from 'rxjs';
+import {TrainingNavigationService} from '../../../core/services/training-navigation.service';
+
+@Component({
+  selector: 'app-training-content-lab',
+  templateUrl: './training-content-lab.component.html',
+  styleUrls: ['./training-content-lab.component.scss']
+})
+export class TrainingContentLabComponent implements OnInit, OnDestroy {
+  private subscriptions: Subscription[] = [];
+
+  constructor(private trainingNavigationService: TrainingNavigationService) { }
+
+  ngOnInit(): void {
+    this.subscriptions.push(this.trainingNavigationService.nextClicked$.subscribe(() => {
+      console.log('TODO IB !!!! nextClicked$ in lab');
+      this.trainingNavigationService.raiseNextContentItem();
+    }));
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(sub => {
+      if (sub) {
+        sub.unsubscribe();
+      }
+    });
+  }
+
+}
