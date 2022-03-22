ad20f2d9a872a9f980895011d10a67148e01a15b
Alexandru Stanciu <alexandru.stanciu@gmail.com>
2022-03-22 11:31:18 +0200

    added test duplicate ids in training files

--- tests/test_skf.py
+++ tests/test_skf.py
@@ -527,10 +527,12 @@ class TestRestPlusApi(unittest.TestCase):
         profiles_dict = json.loads(response.data.decode('utf-8'))
         self.assertIn('profiles', profiles_dict)
         self.assertGreater(len(profiles_dict['profiles']), 0)
+        ids = []
         profiles = profiles_dict['profiles']
         for profile in profiles:
             self.assertEqual(sorted(list(profile.keys())), sorted(['id', 'iconClass', 'name', 'text']))
             profile_id = profile['id']
+            ids.append(profile_id)
             response = self.client.get('/api/training/profile/%s' % profile_id, headers=headers)
             self.assertEqual(response.status_code, 200)
             profile_dict = json.loads(response.data.decode('utf-8'))
@@ -539,6 +541,7 @@ class TestRestPlusApi(unittest.TestCase):
             for course in courses:
                 self.assertEqual(sorted(list(course.keys())), sorted(['id', 'iconClass', 'name', 'text']))
                 course_id = course['id']
+                ids.append(course_id)
                 response = self.client.get('/api/training/course/%s' % course_id, headers=headers)
                 self.assertEqual(response.status_code, 200)
                 course_dict = json.loads(response.data.decode('utf-8'))
@@ -546,6 +549,18 @@ class TestRestPlusApi(unittest.TestCase):
                 for key in ('assetsPath', 'id', 'topics'):
                     self.assertIn(key, course_dict.keys(), "Missing \'%s\' in course %s" % (key, course_id))
                 self.assertTrue(set(course_dict.keys()).issubset(set(['assetsPath', 'content', 'date', 'id', 'languages', 'name', 'topics', 'version'])))
+                for topic in course_dict['topics']:
+                    self.assertIn('id', topic)
+                    topic_id = topic['id']
+                    ids.append(topic_id)
+                    self.assertIn('categories', topic)
+                    for category in topic['categories']:
+                        self.assertIn('id', category)
+                        category_id = category['id']
+                        ids.append(category_id)
+        seen = set()
+        dupes = [x for x in ids if x in seen or seen.add(x)]
+        self.assertEqual(len(dupes), 0, "Duplicate training ids: %s" % str(dupes))
 
 
     #def test_get_description_item(self):
