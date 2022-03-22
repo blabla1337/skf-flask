4bd1b4781843d8daf488cb13329f280a37c1fb96
Alexandru Stanciu <alexandru.stanciu@gmail.com>
2022-03-22 09:05:02 +0200

    add test_training_data

--- tests/test_skf.py
+++ tests/test_skf.py
@@ -519,6 +519,34 @@ class TestRestPlusApi(unittest.TestCase):
         response_dict = json.loads(response.data.decode('utf-8'))
         self.assertEqual(response_dict['message'], "Code example item successfully deleted")
 
+    def test_training_data(self):
+        """Test profile items structure"""
+        headers = {'content-type': 'application/json'}
+        response = self.client.get('/api/training/profiles', headers=headers)
+        self.assertEqual(response.status_code, 200)
+        profiles_dict = json.loads(response.data.decode('utf-8'))
+        self.assertIn('profiles', profiles_dict)
+        self.assertGreater(len(profiles_dict['profiles']), 0)
+        profiles = profiles_dict['profiles']
+        for profile in profiles:
+            self.assertEqual(sorted(list(profile.keys())), sorted(['id', 'iconClass', 'name', 'text']))
+            profile_id = profile['id']
+            response = self.client.get('/api/training/profile/%s' % profile_id, headers=headers)
+            self.assertEqual(response.status_code, 200)
+            profile_dict = json.loads(response.data.decode('utf-8'))
+            self.assertIn('courses', profile_dict)
+            courses = profile_dict['courses']
+            for course in courses:
+                self.assertEqual(sorted(list(course.keys())), sorted(['id', 'iconClass', 'name', 'text']))
+                course_id = course['id']
+                response = self.client.get('/api/training/course/%s' % course_id, headers=headers)
+                self.assertEqual(response.status_code, 200)
+                course_dict = json.loads(response.data.decode('utf-8'))
+                self.assertIsNotNone(course_dict, "Missing course data with id %s" % course_id)
+                for key in ('assetsPath', 'id', 'topics'):
+                    self.assertIn(key, course_dict.keys(), "Missing \'%s\' in course %s" % (key, course_id))
+                self.assertTrue(set(course_dict.keys()).issubset(set(['assetsPath', 'content', 'date', 'id', 'languages', 'name', 'topics', 'version'])))
+
 
     #def test_get_description_item(self):
     #    """Test if the description call is working"""
