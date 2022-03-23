bb51ce9eb823a5b2237bd69080e4293f59425ea6
Ionut Breaz <ibreaz@yahoo.com>
2022-03-23 11:32:50 +0200

    first version of lab

--- Angular2/src/app/pages/training/content-lab/training-content-lab.component.ts
+++ Angular2/src/app/pages/training/content-lab/training-content-lab.component.ts
@@ -1,6 +1,11 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {Subscription} from 'rxjs';
 import {TrainingNavigationService} from '../../../core/services/training-navigation.service';
+import {Lab} from '../../../core/models/course.model';
+import {TrainingService} from '../../../core/services/training.service';
+import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
+
+type CurrentViewType = "None" | "LanguageSelection" | "Lab";
 
 @Component({
   selector: 'app-training-content-lab',
@@ -8,17 +13,49 @@ import {TrainingNavigationService} from '../../../core/services/training-navigat
   styleUrls: ['./training-content-lab.component.scss']
 })
 export class TrainingContentLabComponent implements OnInit, OnDestroy {
+  @Input() lab: Lab;
+  public currentView: CurrentViewType;
+  public selectedLanguageCode: string;
   private subscriptions: Subscription[] = [];
+  public safeLabUrl: SafeResourceUrl;
 
-  constructor(private trainingNavigationService: TrainingNavigationService) { }
+  constructor(private trainingNavigationService: TrainingNavigationService,
+              private trainingService: TrainingService,
+              private domSanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
+    if ((!this.lab.images) || this.lab.images.length === 0) {
+      this.currentView = "None";
+    } else if (this.lab.images.length === 1) {
+      this.showLab(this.trainingService.getLabImageLanguageCode(this.lab.images[0]));
+    } else {
+      this.currentView = "LanguageSelection";
+    }
+
     this.trainingNavigationService.setNextSlideType("None");
 
     this.subscriptions.push(this.trainingNavigationService.nextClicked$.subscribe(() => {
       console.log('TODO IB !!!! nextClicked$ in lab');
       this.trainingNavigationService.raiseNextContentItem();
     }));
+  }
+
+  onLanguageSelected(languageCode: string) {
+    this.showLab(languageCode);
+  }
+
+  showLab(languageCode: string) {
+    this.selectedLanguageCode = languageCode;
+    this.currentView = "Lab"
+    const image = this.lab.images.find(image => image[this.selectedLanguageCode]);
+    const labAddress = image[this.selectedLanguageCode];
+    if (labAddress) {
+      this.subscriptions.push(this.trainingService.getLabUrl(labAddress).subscribe(labUrl => {
+        this.safeLabUrl = this.domSanitizer.bypassSecurityTrustResourceUrl(labUrl);
+      }))
+    } else {
+      console.error("Lab has no valid address");
+    }
   }
 
   ngOnDestroy(): void {
@@ -28,5 +65,4 @@ export class TrainingContentLabComponent implements OnInit, OnDestroy {
       }
     });
   }
-
 }
