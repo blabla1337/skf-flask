971dfe8340ecc6e4d5d215274cc1b91c6f43951e
Alexandru Stanciu <alexandru.stanciu@gmail.com>
2022-03-28 19:22:47 +0300

    Fix interactive_labs post endpoints

--- skf/api/labs/endpoints/lab_delete.py
+++ skf/api/labs/endpoints/lab_delete.py
@@ -13,9 +13,10 @@ ns = api.namespace('interactive_labs', description='Operations related to the la
 @api.response(404, 'Validation error', message)
 class LabDelete(Resource):
 
-    @api.marshal_with(lab_user_id)
+    @api.expect(lab_user_id)
     @api.response(400, 'No results found', message)
     def post(self, instance_id):
+        data = request.json
         val_num(instance_id)
         val_alpha_num_special(data.get('user_id'))
         result = delete_labs(instance_id, data.get('user_id'))
