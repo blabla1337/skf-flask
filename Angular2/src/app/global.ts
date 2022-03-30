705c74df88e2a14fece7b656af0132f122c107e9
blabla1337 <glenntencate@gmail.com>
2022-03-31 00:00:04 +0200

    fix project view

--- Angular2/src/app/global.ts
+++ Angular2/src/app/global.ts
@@ -3,7 +3,7 @@ export class AppSettings
    public static AUTH_TOKEN = localStorage.getItem('auth_token');
    public static USER = localStorage.getItem('user');
    public static SKIP_LOGIN = localStorage.getItem('skip_login');
-   public static USER_PRIV = localStorage.getItem('privilege')
+   public static USER_PRIV = localStorage.getItem('privilege');
 }
 
 
--- Angular2/src/app/layouts/horizontal/horizontal.component.ts
+++ Angular2/src/app/layouts/horizontal/horizontal.component.ts
@@ -21,6 +21,11 @@ export class HorizontalComponent implements OnInit, AfterViewInit {
     document.body.setAttribute('data-topbar', 'dark');
 
     this.userService.getJwtUserId();
+    
+    const priv_setting = localStorage.getItem("privilege");
+    if (priv_setting === undefined || priv_setting === "" || priv_setting === null ) {
+      localStorage.setItem('privilege','manage');
+    }
 
     const theme_var = localStorage.getItem("theme");
     if (theme_var === undefined || theme_var === "" || theme_var === null ) {
